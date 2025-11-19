871794748af737503b16626332af0d97113de98f
Samuel Dalvai <s.dalvai@ewo.com>
2025-11-19 22:00:00 +0100

    feat: add class to handle forces

---
+++ src/physics/Force.ts
@@ -0,0 +1,57 @@
+import Particle from './Particle';
+import Vec2 from './Vec2';
+
+export default class Force {
+    static generateDragForce = (particle: Particle, k: number): Vec2 => {
+        let dragForce = new Vec2(0, 0);
+
+        if (particle.velocity.magnitudeSquared() > 0) {
+            // Calculate the drag direction (inverse of velocity unit vector)
+            const dragDirection = particle.velocity.unitVector().scaleNew(-1);
+
+            // Calculate the drag magnitude, k * |v|^2
+            const dragMagnitude = k * particle.velocity.magnitudeSquared();
+            // float dragMagnitude = k * particle.velocity.MagnitudeSquared();
+
+            // Generate the final drag force with direction and magnitude
+            dragForce = dragDirection.scaleNew(dragMagnitude);
+        }
+
+        return dragForce;
+    };
+    static generateFrictionForce = (particle: Particle, k: number): Vec2 => {
+        // Calculate the friction direction (inverse of velocity unit vector)
+        const frictionDirection = particle.velocity.unitVector().scaleNew(-1);
+
+        // Calculate the friction magnitude (just k, for now)
+        const frictionMagnitude = k;
+
+        // Calculate the final resulting friction force vector
+        return frictionDirection.scaleNew(frictionMagnitude);
+    };
+
+    static generateGravitationalForce = (
+        a: Particle,
+        b: Particle,
+        G: number,
+        minDistance: number,
+        maxDistance: number,
+    ): Vec2 => {
+        // Calculate the distance between the two objects
+        const d = b.position.subNew(a.position);
+
+        let distanceSquared = d.magnitudeSquared();
+
+        // Clamp the values of the distance (to allow for some insteresting visual effects)
+        distanceSquared = Math.min(Math.max(distanceSquared, minDistance), maxDistance);
+
+        // Calculate the direction of the attraction force
+        const attractionDirection = d.unitVector();
+
+        // Calculate the strength of the attraction force
+        const attractionMagnitude = (G * (a.mass * b.mass)) / distanceSquared;
+
+        // Calculate the final resulting attraction force vector
+        return attractionDirection.scaleNew(attractionMagnitude);
+    };
+}
