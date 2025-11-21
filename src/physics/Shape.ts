6d986dfc1afdc5ad19267b18647cc92f36e131f9
Samuel Dalvai <s.dalvai@ewo.com>
2025-11-21 22:00:00 +0100

    feat: add box shape special polygon

--- src/physics/Shape.ts
+++ src/physics/Shape.ts
@@ -34,7 +34,7 @@ export class PolygonShape extends Shape {
     localVertices: Vec2[] = [];
     worldVertices: Vec2[] = [];
 
-    constructor(vertices: Vec2[]) {
+    constructor() {
         super();
         // TODO: ....
     }
@@ -54,5 +54,36 @@ export class PolygonShape extends Shape {
             this.worldVertices[i] = this.localVertices[i].rotate(angle);
             this.worldVertices[i].addAssign(position);
         }
+    };
+}
+
+export class BoxShape extends PolygonShape {
+    width: number;
+    height: number;
+
+    constructor(width: number, height: number) {
+        super();
+        this.width = width;
+        this.height = height;
+
+        this.localVertices.push(new Vec2(-width / 2.0, -height / 2.0));
+        this.localVertices.push(new Vec2(+width / 2.0, -height / 2.0));
+        this.localVertices.push(new Vec2(+width / 2.0, +height / 2.0));
+        this.localVertices.push(new Vec2(-width / 2.0, +height / 2.0));
+
+        this.worldVertices.push(new Vec2(-width / 2.0, -height / 2.0));
+        this.worldVertices.push(new Vec2(+width / 2.0, -height / 2.0));
+        this.worldVertices.push(new Vec2(+width / 2.0, +height / 2.0));
+        this.worldVertices.push(new Vec2(-width / 2.0, +height / 2.0));
+    }
+
+    getType = (): ShapeType => {
+        return ShapeType.BOX;
+    };
+    
+    getMomentOfInertia = (): number => {
+        // For a rectangle, the moment of inertia is 1/12 * (w^2 + h^2)
+        // But this still needs to be multiplied by the rigidbody's mass
+        return 0.083333 * (this.width * this.width + this.height * this.height);
     };
 }
