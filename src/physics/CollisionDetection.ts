dbbcadb8eae84c5e6667bcb64f0491c70dabd484
Samuel Dalvai <s.dalvai@ewo.com>
2025-11-24 21:47:38 +0100

    feat: implement min separation algorithm and polygon-polygon collision

--- src/physics/CollisionDetection.ts
+++ src/physics/CollisionDetection.ts
@@ -1,16 +1,24 @@
 import Body from './Body';
 import Contact from './Contact';
-import { CircleShape, ShapeType } from './Shape';
+import { CircleShape, PolygonShape, ShapeType } from './Shape';
+import Vec2 from './Vec2';
 
 export default class CollisionDetection {
     static isColliding = (a: Body, b: Body, contact: Contact): boolean => {
         const aIsCircle = a.shape.getType() === ShapeType.CIRCLE;
         const bIsCircle = b.shape.getType() === ShapeType.CIRCLE;
+        const aIsPolygon = a.shape.getType() === ShapeType.POLYGON || a.shape.getType() === ShapeType.BOX;
+        const bIsPolygon = b.shape.getType() === ShapeType.POLYGON || b.shape.getType() === ShapeType.BOX;
 
         if (aIsCircle && bIsCircle) {
             return this.isCollidingCircleCircle(a, b, contact);
         }
 
+        if (aIsPolygon && bIsPolygon) {
+            return this.isCollidingPolygonPolygon(a, b, contact);
+        }
+
+        // TODO: check circle with polygon collision
         return false;
     };
 
@@ -40,6 +48,39 @@ export default class CollisionDetection {
         return true;
     };
 
-    // TODO: static isCollidingPolygonPolygon = (a: Body, b: Body): boolean => {return false};
+    static isCollidingPolygonPolygon = (a: Body, b: Body, contact: Contact): boolean => {
+        const aPolygonShape = a.shape as PolygonShape;
+        const bPolygonShape = b.shape as PolygonShape;
+        const aAxis = new Vec2();
+        const bAxis = new Vec2();
+        const aPoint = new Vec2();
+        const bPoint = new Vec2();
+
+        const abSeparation = aPolygonShape.findMinSeparation(bPolygonShape, aAxis, aPoint);
+        if (abSeparation >= 0) {
+            return false;
+        }
+
+        const baSeparation = bPolygonShape.findMinSeparation(aPolygonShape, bAxis, bPoint);
+        if (baSeparation >= 0) {
+            return false;
+        }
+
+        contact.a = a;
+        contact.b = b;
+        if (abSeparation > baSeparation) {
+            contact.depth = -abSeparation;
+            contact.normal = aAxis.normal();
+            contact.start = aPoint;
+            contact.end = aPoint.addNew(contact.normal.scaleNew(contact.depth));
+        } else {
+            contact.depth = -baSeparation;
+            contact.normal = bAxis.normal().negate();
+            contact.start = bPoint.subNew(contact.normal.scaleNew(contact.depth));
+            contact.end = bPoint;
+        }
+        return true;
+    };
+
     // TODO: static isCollidingPolygonCircle(a: Body, b: Body): boolean => {return false};
 }
