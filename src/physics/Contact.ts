e7468b05edc15c5156d672423c4f55022612c3b5
Samuel Dalvai <s.dalvai@ewo.com>
2025-11-24 10:38:53 +0100

    feat: add blueprints for collision detection and resolution

---
+++ src/physics/Contact.ts
@@ -0,0 +1,51 @@
+export default class Contact {
+    // Body* a;
+    // Body* b;
+
+    // Vec2 start;
+    // Vec2 end;
+
+    // Vec2 normal;
+    // float depth;
+
+    // Contact() = default;
+    // ~Contact() = default;
+
+    // Resolves the collision using the position change method
+    // void Contact::ResolvePenetration() {
+    //     if (a->IsStatic() && b->IsStatic()) {
+    //         return;
+    //     }
+
+    //     float da = depth / (a->invMass + b->invMass) * a->invMass;
+    //     float db = depth / (a->invMass + b->invMass) * b->invMass;
+        
+    //     a->position -= normal * da;
+    //     b->position += normal * db;
+    // }
+
+    // Resolves the collision using the impulse method
+    // void Contact::ResolveCollision() {
+    //     // Apply positional correction using the projection method
+    //     ResolvePenetration();
+        
+    //     // Define elasticity (coefficient of restitution e)
+    //     float e = std::min(a->restitution, b->restitution);
+        
+    //     // Calculate the relative velocity between the two objects
+    //     const Vec2 vrel = (a->velocity - b->velocity);
+
+    //     // Calculate the relative velocity along the normal collision vector
+    //     float vrelDotNormal = vrel.Dot(normal);
+
+    //     // Now we proceed to calculate the collision impulse
+    //     const Vec2 impulseDirection = normal;
+    //     const float impulseMagnitude = -(1 + e) * vrelDotNormal / (a->invMass + b->invMass);
+        
+    //     Vec2 jn = impulseDirection * impulseMagnitude;
+        
+    //     // Apply the impulse vector to both objects in opposite direction
+    //     a->ApplyImpulse(jn);
+    //     b->ApplyImpulse(-jn);
+    // }
+}
