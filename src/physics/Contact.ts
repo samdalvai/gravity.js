7ff7e8d4a554bf4846039496be595bebc1abd5cd
Samuel Dalvai <s.dalvai@ewo.com>
2025-11-24 22:00:00 +0100

    feat: implement contact logic with rotation

--- src/physics/Contact.ts
+++ src/physics/Contact.ts
@@ -33,7 +33,7 @@ export default class Contact {
 
     // Resolves the collision using the impulse method
     resolveCollision = (): void => {
-        if (!this.a || !this.b || !this.depth || !this.normal) {
+        if (!this.a || !this.b || !this.depth || !this.normal || !this.start || !this.end) {
             console.error('Some Contact variables are not initialized: ', this);
             throw new Error('Some Contact variables are not initialized');
         }
@@ -41,24 +41,45 @@ export default class Contact {
         // Apply positional correction using the projection method
         this.resolvePenetration();
 
-        // Define elasticity (coefficient of restitution e)
+        // Define coefficient of restitution (elasticity) and friction
         const e = Math.min(this.a.restitution, this.b.restitution);
+        const f = Math.min(this.a.friction, this.b.friction);
 
         // Calculate the relative velocity between the two objects
-        const vrel = this.a.velocity.subNew(this.b.velocity);
+        const ra = this.end.subNew(this.a.position);
+        const rb = this.start.subNew(this.b.position);
+        const va = this.a.velocity.addNew(new Vec2(-this.a.angularVelocity * ra.y, this.a.angularVelocity * ra.x));
+        const vb = this.b.velocity.addNew(new Vec2(-this.b.angularVelocity * rb.y, this.b.angularVelocity * rb.x));
+        const vrel = va.subNew(vb);
 
-        // Calculate the relative velocity along the normal collision vector
+        // Now we proceed to calculate the collision impulse along the normal
         const vrelDotNormal = vrel.dot(this.normal);
+        const impulseDirectionN = this.normal;
+        const impulseMagnitudeN =
+            (-(1 + e) * vrelDotNormal) /
+            (this.a.invMass +
+                this.b.invMass +
+                ra.cross(this.normal) * ra.cross(this.normal) * this.a.invI +
+                rb.cross(this.normal) * rb.cross(this.normal) * this.b.invI);
+        const jN = impulseDirectionN.scaleNew(impulseMagnitudeN);
 
-        // Now we proceed to calculate the collision impulse
-        // TODO: why we assign normal to impulseDirection? Is this needed?
-        const impulseDirection = this.normal;
-        const impulseMagnitude = (-(1 + e) * vrelDotNormal) / (this.a.invMass + this.b.invMass);
+        // Now we proceed to calculate the collision impulse along the tangent
+        const tangent = this.normal.normal();
+        const vrelDotTangent = vrel.dot(tangent);
+        const impulseDirectionT = tangent;
+        const impulseMagnitudeT =
+            (f * -(1 + e) * vrelDotTangent) /
+            (this.a.invMass +
+                this.b.invMass +
+                ra.cross(tangent) * ra.cross(tangent) * this.a.invI +
+                rb.cross(tangent) * rb.cross(tangent) * this.b.invI);
+        const jT = impulseDirectionT.scaleNew(impulseMagnitudeT);
 
-        const jn = impulseDirection.scaleNew(impulseMagnitude);
+        // Calculate the final impulse j combining normal and tangent impulses
+        const j = jN.addNew(jT);
 
         // Apply the impulse vector to both objects in opposite direction
-        this.a.applyLinearImpulse(jn);
-        this.b.applyLinearImpulse(jn.negate());
+        this.a.applyAngularImpulse(j, ra);
+        this.b.applyAngularImpulse(j.negate(), rb);
     };
 }
