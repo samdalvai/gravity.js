969bd8c1e1b1e1f799d79a3878872e98462cd910
Samuel Dalvai <s.dalvai@ewo.com>
2025-11-24 22:35:20 +0100

    feat: implement collision detection between polygons and circles

--- src/physics/CollisionDetection.ts
+++ src/physics/CollisionDetection.ts
@@ -18,7 +18,14 @@ export default class CollisionDetection {
             return this.isCollidingPolygonPolygon(a, b, contact);
         }
 
-        // TODO: check circle with polygon collision
+        if (aIsPolygon && bIsCircle) {
+            return this.isCollidingPolygonCircle(a, b, contact);
+        }
+
+        if (aIsCircle && bIsPolygon) {
+            return this.isCollidingPolygonCircle(b, a, contact);
+        }
+
         return false;
     };
 
@@ -51,7 +58,7 @@ export default class CollisionDetection {
     static isCollidingPolygonPolygon = (a: Body, b: Body, contact: Contact): boolean => {
         const aPolygonShape = a.shape as PolygonShape;
         const bPolygonShape = b.shape as PolygonShape;
-        
+
         const aAxis = new Vec2();
         const bAxis = new Vec2();
         const aPoint = new Vec2();
@@ -83,5 +90,111 @@ export default class CollisionDetection {
         return true;
     };
 
-    // TODO: static isCollidingPolygonCircle(a: Body, b: Body): boolean => {return false};
+    static isCollidingPolygonCircle = (polygon: Body, circle: Body, contact: Contact): boolean => {
+        const polygonShape = polygon.shape as PolygonShape;
+        const circleShape = circle.shape as CircleShape;
+        const polygonVertices = polygonShape.worldVertices;
+
+        let isOutside = false;
+        let minCurrVertex = new Vec2();
+        let minNextVertex = new Vec2();
+        let distanceCircleEdge = -Infinity;
+
+        // Loop all the edges of the polygon/box finding the nearest edge to the circle center
+        for (let i = 0; i < polygonVertices.length; i++) {
+            const currVertex = i;
+            const nextVertex = (i + 1) % polygonVertices.length;
+            const edge = polygonShape.edgeAt(currVertex);
+            const normal = edge.normal();
+
+            // Compare the circle center with the rectangle vertex
+            const vertexToCircleCenter = circle.position.subNew(polygonVertices[currVertex]);
+            const projection = vertexToCircleCenter.dot(normal);
+
+            // If we found a dot product projection that is in the positive/outside side of the normal
+            if (projection > 0) {
+                // Circle center is outside the polygon
+                distanceCircleEdge = projection;
+                minCurrVertex = polygonVertices[currVertex];
+                minNextVertex = polygonVertices[nextVertex];
+                isOutside = true;
+                break;
+            } else {
+                // Circle center is inside the rectangle, find the min edge (the one with the least negative projection)
+                if (projection > distanceCircleEdge) {
+                    distanceCircleEdge = projection;
+                    minCurrVertex = polygonVertices[currVertex];
+                    minNextVertex = polygonVertices[nextVertex];
+                }
+            }
+        }
+
+        if (isOutside) {
+            ///////////////////////////////////////
+            // Check if we are inside region A:
+            ///////////////////////////////////////
+            let v1 = circle.position.subNew(minCurrVertex); // vector from the nearest vertex to the circle center
+            let v2 = minNextVertex.subNew(minCurrVertex); // the nearest edge (from curr vertex to next vertex)
+            if (v1.dot(v2) < 0) {
+                // Distance from vertex to circle center is greater than radius... no collision
+                if (v1.magnitude() > circleShape.radius) {
+                    return false;
+                } else {
+                    // Detected collision in region A:
+                    contact.a = polygon;
+                    contact.b = circle;
+                    contact.depth = circleShape.radius - v1.magnitude();
+                    contact.normal = v1.normalize();
+                    contact.start = circle.position.addNew(contact.normal.scaleNew(-circleShape.radius));
+                    contact.end = contact.start.addNew(contact.normal.scaleNew(contact.depth));
+                }
+            } else {
+                ///////////////////////////////////////
+                // Check if we are inside region B:
+                ///////////////////////////////////////
+                v1 = circle.position.subNew(minNextVertex); // vector from the next nearest vertex to the circle center
+                v2 = minCurrVertex.subNew(minNextVertex); // the nearest edge
+                if (v1.dot(v2) < 0) {
+                    // Distance from vertex to circle center is greater than radius... no collision
+                    if (v1.magnitude() > circleShape.radius) {
+                        return false;
+                    } else {
+                        // Detected collision in region B:
+                        contact.a = polygon;
+                        contact.b = circle;
+                        contact.depth = circleShape.radius - v1.magnitude();
+                        contact.normal = v1.normalize();
+                        contact.start = circle.position.addNew(contact.normal.scaleNew(-circleShape.radius));
+                        contact.end = contact.start.addNew(contact.normal.scaleNew(contact.depth));
+                    }
+                } else {
+                    ///////////////////////////////////////
+                    // We are inside region C:
+                    ///////////////////////////////////////
+                    if (distanceCircleEdge > circleShape.radius) {
+                        // No collision... Distance between the closest distance and the circle center is greater than the radius.
+                        return false;
+                    } else {
+                        // Detected collision in region C:
+                        contact.a = polygon;
+                        contact.b = circle;
+                        contact.depth = circleShape.radius - distanceCircleEdge;
+                        contact.normal = minNextVertex.subNew(minCurrVertex).normal();
+                        contact.start = circle.position.subNew(contact.normal.scaleNew(circleShape.radius));
+                        contact.end = contact.start.addNew(contact.normal.scaleNew(contact.depth));
+                    }
+                }
+            }
+        } else {
+            // The center of circle is inside the polygon... it is definitely colliding!
+            contact.a = polygon;
+            contact.b = circle;
+            contact.depth = circleShape.radius - distanceCircleEdge;
+            contact.normal = minNextVertex.subNew(minCurrVertex).normal();
+            contact.start = circle.position.subNew(contact.normal.scaleNew(circleShape.radius));
+            contact.end = contact.start.addNew(contact.normal.scaleNew(contact.depth));
+        }
+
+        return true;
+    };
 }
