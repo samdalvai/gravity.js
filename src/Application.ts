d7f78563a4b5e2ce2e33aaae2666f304ca29dffb
Samuel Dalvai <s.dalvai@ewo.com>
2025-11-24 18:10:29 +0100

    feat: render circles in Application.ts

--- src/Application.ts
+++ src/Application.ts
@@ -1,7 +1,7 @@
 import Graphics from './Graphics';
 import InputManager from './InputManager';
 import Body from './physics/Body';
-import { CircleShape } from './physics/Shape';
+import { BoxShape, CircleShape, ShapeType } from './physics/Shape';
 
 export default class Application {
     private running: boolean;
@@ -61,11 +61,11 @@ export default class Application {
 
             switch (inputEvent.type) {
                 case 'mousedown':
-                    // int x, y;
-                    // SDL_GetMouseState(&x, &y);
-                    // Body* smallBall = new Body(CircleShape(40), x, y, 1.0);
-                    // smallBall->restitution = 0.2;
-                    // bodies.push_back(smallBall);
+                    {
+                        const smallBall = new Body(new CircleShape(40), inputEvent.x, inputEvent.y, 1);
+                        smallBall.restitution = 0.2;
+                        this.bodies.push(smallBall);
+                    }
                     break;
                 case 'mouseup':
                     break;
@@ -79,21 +79,17 @@ export default class Application {
         // Apply a drag force
         // const drag = Force.generateDragForce(particle, 0.003);
         // particle.addForce(drag);
-        
         //     // Apply the weight force
         //     Vec2 weight = Vec2(0.0, body->mass * 9.8 * PIXELS_PER_METER);
         //     body->AddForce(weight);
-
         //     // Apply the wind force
         //     Vec2 wind = Vec2(2.0 * PIXELS_PER_METER, 0.0);
         //     body->AddForce(wind);
         // }
-
         // // Integrate the acceleration and velocity to estimate the new position
         // for (auto body: bodies) {
         //     body->Update(deltaTime);
         // }
-
         // // Check all the rigidbodies with the other rigidbodies for collision
         // for (int i = 0; i <= bodies.size() - 1; i++) {
         //     for (int j = i + 1; j < bodies.size(); j++) {
@@ -101,13 +97,10 @@ export default class Application {
         //         Body* b = bodies[j];
         //         a->isColliding = false;
         //         b->isColliding = false;
-                
         //         Contact contact;
-
         //         if (CollisionDetection::IsColliding(a, b, contact)) {
         //             // Resolve the collision using the impulse method
         //             contact.ResolveCollision();
-
         //             // Draw debug contact information
         //             Graphics::DrawFillCircle(contact.start.x, contact.start.y, 3, 0xFFFF00FF);
         //             Graphics::DrawFillCircle(contact.end.x, contact.end.y, 3, 0xFFFF00FF);
@@ -117,7 +110,6 @@ export default class Application {
         //         }
         //     }
         // }
-
         // // Check the boundaries of the window applying a hardcoded bounce flip in velocity
         // for (auto body: bodies) {
         //     if (body->shape->GetType() == CIRCLE) {
@@ -141,21 +133,22 @@ export default class Application {
     };
 
     render = (): void => {
-        // Draw all bodies
-        // for (auto body: bodies) {
-        //     Uint32 color = body->isColliding ? 0xFF0000FF : 0xFFFFFFFF;
-
-        //     if (body->shape->GetType() == CIRCLE) {
-        //         CircleShape* circleShape = (CircleShape*) body->shape;
-        //         Graphics::DrawFillCircle(body->position.x, body->position.y, circleShape->radius, 0xFFFFFFFF);
-        //     }
-        //     if (body->shape->GetType() == BOX) {
-        //         BoxShape* boxShape = (BoxShape*) body->shape;
-        //         Graphics::DrawPolygon(body->position.x, body->position.y, boxShape->worldVertices, 0xFFFFFFFF);
-        //     }
-        // }
-
         Graphics.clearScreen();
+
+        // Draw all bodies
+        for (const body of this.bodies) {
+            const color = body.isColliding ? 'red' : 'white';
+
+            if (body.shape.getType() === ShapeType.CIRCLE) {
+                const circleShape = body.shape as CircleShape;
+                Graphics.drawFillCircle(body.position.x, body.position.y, circleShape.radius, 'white');
+            }
+            if (body.shape.getType() === ShapeType.BOX) {
+                const boxShape = body.shape as BoxShape;
+                // TODO: to be implemented
+                // Graphics.drawPolygon(body.position.x, body.position.y, boxShape.worldVertices, "white");
+            }
+        }
     };
 
     sleep = (milliseconds: number) => {
