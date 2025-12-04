316fe68e361d70a1153b15bbb9b9f8aeb9b81394
Samuel Dalvai <samuel.dalvai@gmail.com>
2025-12-04 22:00:00 +0100

    feat: set application to debug triangle collision

--- __tests__/physics/CollisionDetection.test.ts
+++ __tests__/physics/CollisionDetection.test.ts
@@ -1,6 +1,7 @@
 import Body from '../../src/physics/Body';
 import CollisionDetection from '../../src/physics/CollisionDetection';
-import { BoxShape, CircleShape } from '../../src/physics/Shape';
+import { BoxShape, CircleShape, PolygonShape } from '../../src/physics/Shape';
+import Vec2 from '../../src/physics/Vec2';
 
 describe('CollisionDetection', () => {
     test('detectCollisionCircleCircle() detects collision between fully overlapped circles', () => {
@@ -153,4 +154,70 @@ describe('CollisionDetection', () => {
         expect(collisionResult.isColliding).toBe(false);
         expect(collisionResult.contacts).toBeUndefined();
     });
+
+    test('detectCollisionPolygonPolygon() detects collision between fully overlapped triangles', () => {
+        const triangleVertices = [new Vec2(30, 30), new Vec2(-30, 30), new Vec2(0, -30)];
+        const a = new Body(new PolygonShape(triangleVertices), 0, 0, 1.0);
+        const b = new Body(new PolygonShape(triangleVertices), 0, 0, 1.0);
+
+        const collisionResult = CollisionDetection.detectCollisionPolygonPolygon(a, b);
+
+        expect(collisionResult.isColliding).toBe(true);
+        expect(collisionResult.contacts).not.toBeUndefined();
+        expect(collisionResult.contacts).toHaveLength(2);
+        expect(collisionResult.contacts![0].depth).toBe(0);
+        expect(collisionResult.contacts![1].depth).toBeLessThanOrEqual(60);
+    });
+
+    test('detectCollisionPolygonPolygon() detects collision between half overlapped triangles', () => {
+        const triangleVerticesA = [new Vec2(30, 30), new Vec2(-30, 30), new Vec2(0, -30)];
+        const triangleVerticesB = [new Vec2(30, 30), new Vec2(-30, 30), new Vec2(0, -30)];
+        const a = new Body(new PolygonShape(triangleVerticesA), 0, 0, 1.0);
+        const b = new Body(new PolygonShape(triangleVerticesB), 0, 15, 1.0);
+
+        console.log(a.shape);
+        console.log(b.shape);
+        
+
+        const collisionResult = CollisionDetection.detectCollisionPolygonPolygon(a, b);
+
+        expect(collisionResult.isColliding).toBe(true);
+        expect(collisionResult.contacts).not.toBeUndefined();
+        // expect(collisionResult.contacts).toHaveLength(2);
+        // expect(collisionResult.contacts![0].depth).toBe(30);
+        // expect(collisionResult.contacts![1].depth).toBe(30);
+    });
+
+    // test('detectCollisionPolygonPolygon() detects collision between triangles that overlap by a quarter', () => {
+    //     const a = new Body(new BoxShape(60, 60), 0, 0, 1.0);
+    //     const b = new Body(new BoxShape(60, 60), 30, 30, 1.0);
+
+    //     const collisionResult = CollisionDetection.detectCollisionPolygonPolygon(a, b);
+
+    //     expect(collisionResult.isColliding).toBe(true);
+    //     expect(collisionResult.contacts).not.toBeUndefined();
+    //     expect(collisionResult.contacts).toHaveLength(2);
+    //     expect(collisionResult.contacts![0].depth).toBe(30);
+    //     expect(collisionResult.contacts![1].depth).toBe(30);
+    // });
+
+    // test('detectCollisionPolygonPolygon() should not detect collision for not overlapped triangles', () => {
+    //     const a = new Body(new BoxShape(60, 60), 0, 0, 1.0);
+    //     const b = new Body(new BoxShape(60, 60), 200, 200, 1.0);
+
+    //     const collisionResult = CollisionDetection.detectCollisionPolygonPolygon(a, b);
+
+    //     expect(collisionResult.isColliding).toBe(false);
+    //     expect(collisionResult.contacts).toBeUndefined();
+    // });
+
+    // test('detectCollisionPolygonPolygon() should not detect collision for not overlapped and touching triangles', () => {
+    //     const a = new Body(new BoxShape(60, 60), 0, 0, 1.0);
+    //     const b = new Body(new BoxShape(60, 60), 60, 60, 1.0);
+
+    //     const collisionResult = CollisionDetection.detectCollisionPolygonPolygon(a, b);
+
+    //     expect(collisionResult.isColliding).toBe(false);
+    //     expect(collisionResult.contacts).toBeUndefined();
+    // });
 });
--- src/Application.ts
+++ src/Application.ts
@@ -62,123 +62,131 @@ export default class Application {
 
         this.running = Graphics.openWindow();
 
-        this.bgTexture = AssetStore.getTexture('background');
+        // this.bgTexture = AssetStore.getTexture('background');
 
-        // Add bird
-        const bird = new Body(new CircleShape(45), 100, Graphics.height() / 2.0 + 220, 3.0);
-        bird.setTexture('birdRed');
-        this.world.addBody(bird);
+        // // Add bird
+        // const bird = new Body(new CircleShape(45), 100, Graphics.height() / 2.0 + 220, 3.0);
+        // bird.setTexture('birdRed');
+        // this.world.addBody(bird);
 
-        // Add a floor and walls to contain objects objects
-        const floor = new Body(
-            new BoxShape(Graphics.width() - 50, 50),
-            Graphics.width() / 2.0,
-            Graphics.height() / 2.0 + 340,
-            0.0,
-        );
-        const leftFence = new Body(new BoxShape(50, Graphics.height() - 75), 0, Graphics.height() / 2.0 - 35, 0.0);
-        const rightFence = new Body(
-            new BoxShape(50, Graphics.height() - 75),
-            Graphics.width(),
-            Graphics.height() / 2.0 - 35,
-            0.0,
-        );
-        this.world.addBody(floor);
-        this.world.addBody(leftFence);
-        this.world.addBody(rightFence);
+        // // Add a floor and walls to contain objects objects
+        // const floor = new Body(
+        //     new BoxShape(Graphics.width() - 50, 50),
+        //     Graphics.width() / 2.0,
+        //     Graphics.height() / 2.0 + 340,
+        //     0.0,
+        // );
+        // const leftFence = new Body(new BoxShape(50, Graphics.height() - 75), 0, Graphics.height() / 2.0 - 35, 0.0);
+        // const rightFence = new Body(
+        //     new BoxShape(50, Graphics.height() - 75),
+        //     Graphics.width(),
+        //     Graphics.height() / 2.0 - 35,
+        //     0.0,
+        // );
+        // this.world.addBody(floor);
+        // this.world.addBody(leftFence);
+        // this.world.addBody(rightFence);
 
-        // Add a stack of boxes
-        for (let i = 1; i <= 4; i++) {
-            const mass = 10.0 / i;
-            const box = new Body(new BoxShape(50, 50), 600, floor.position.y - i * 55, mass);
-            box.setTexture('woodBox');
-            box.friction = 0.9;
-            box.restitution = 0.1;
-            this.world.addBody(box);
-        }
+        // // Add a stack of boxes
+        // for (let i = 1; i <= 4; i++) {
+        //     const mass = 10.0 / i;
+        //     const box = new Body(new BoxShape(50, 50), 600, floor.position.y - i * 55, mass);
+        //     box.setTexture('woodBox');
+        //     box.friction = 0.9;
+        //     box.restitution = 0.1;
+        //     this.world.addBody(box);
+        // }
 
-        // Add structure with blocks
-        const plank1 = new Body(new BoxShape(50, 150), Graphics.width() / 2.0 + 20, floor.position.y - 100, 5.0);
-        const plank2 = new Body(new BoxShape(50, 150), Graphics.width() / 2.0 + 180, floor.position.y - 100, 5.0);
-        const plank3 = new Body(new BoxShape(250, 25), Graphics.width() / 2.0 + 100, floor.position.y - 200, 2.0);
-        plank1.setTexture('woodPlankSolid');
-        plank2.setTexture('woodPlankSolid');
-        plank3.setTexture('woodPlankCracked');
-        this.world.addBody(plank1);
-        this.world.addBody(plank2);
-        this.world.addBody(plank3);
+        // // Add structure with blocks
+        // const plank1 = new Body(new BoxShape(50, 150), Graphics.width() / 2.0 + 20, floor.position.y - 100, 5.0);
+        // const plank2 = new Body(new BoxShape(50, 150), Graphics.width() / 2.0 + 180, floor.position.y - 100, 5.0);
+        // const plank3 = new Body(new BoxShape(250, 25), Graphics.width() / 2.0 + 100, floor.position.y - 200, 2.0);
+        // plank1.setTexture('woodPlankSolid');
+        // plank2.setTexture('woodPlankSolid');
+        // plank3.setTexture('woodPlankCracked');
+        // this.world.addBody(plank1);
+        // this.world.addBody(plank2);
+        // this.world.addBody(plank3);
 
-        // Add a triangle polygon
-        const triangleVertices = [new Vec2(30, 30), new Vec2(-30, 30), new Vec2(0, -30)];
-        const triangle = new Body(new PolygonShape(triangleVertices), plank3.position.x, plank3.position.y - 50, 0.5);
-        triangle.setTexture('woodTriangle');
-        this.world.addBody(triangle);
+        // // Add a triangle polygon
+        // const triangleVertices = [new Vec2(30, 30), new Vec2(-30, 30), new Vec2(0, -30)];
+        // const triangle = new Body(new PolygonShape(triangleVertices), plank3.position.x, plank3.position.y - 50, 0.5);
+        // triangle.setTexture('woodTriangle');
+        // this.world.addBody(triangle);
 
-        // Add a pyramid of boxes
-        const numRows = 5;
-        for (let col = 0; col < numRows; col++) {
-            for (let row = 0; row < col; row++) {
-                const x = plank3.position.x + 200 + col * 50 - row * 25;
-                const y = floor.position.y - 50 - row * 52;
-                const mass = 5 / (row + 1);
-                const box = new Body(new BoxShape(50, 50), x, y, mass);
-                box.friction = 0.9;
-                box.restitution = 0.0;
-                box.setTexture('woodBox');
-                this.world.addBody(box);
-            }
-        }
+        // // Add a pyramid of boxes
+        // const numRows = 5;
+        // for (let col = 0; col < numRows; col++) {
+        //     for (let row = 0; row < col; row++) {
+        //         const x = plank3.position.x + 200 + col * 50 - row * 25;
+        //         const y = floor.position.y - 50 - row * 52;
+        //         const mass = 5 / (row + 1);
+        //         const box = new Body(new BoxShape(50, 50), x, y, mass);
+        //         box.friction = 0.9;
+        //         box.restitution = 0.0;
+        //         box.setTexture('woodBox');
+        //         this.world.addBody(box);
+        //     }
+        // }
 
-        // Add a bridge of connected steps and joints
-        const numSteps = 10;
-        const spacing = 33;
+        // // Add a bridge of connected steps and joints
+        // const numSteps = 10;
+        // const spacing = 33;
 
-        // Start anchor (static)
-        const startStep = new Body(new BoxShape(80, 20), 200, 200, 0.0);
-        startStep.setTexture('rockBridgeAnchor');
-        this.world.addBody(startStep);
+        // // Start anchor (static)
+        // const startStep = new Body(new BoxShape(80, 20), 200, 200, 0.0);
+        // startStep.setTexture('rockBridgeAnchor');
+        // this.world.addBody(startStep);
 
-        // The first connection should be from the anchor, not the floor
-        let last = startStep;
+        // // The first connection should be from the anchor, not the floor
+        // let last = startStep;
 
-        for (let i = 1; i <= numSteps; i++) {
-            const x = startStep.position.x + 30 + i * spacing;
-            const y = startStep.position.y + 20;
-            const mass = i == numSteps ? 0.0 : 3.0;
+        // for (let i = 1; i <= numSteps; i++) {
+        //     const x = startStep.position.x + 30 + i * spacing;
+        //     const y = startStep.position.y + 20;
+        //     const mass = i == numSteps ? 0.0 : 3.0;
 
-            const step = new Body(new CircleShape(15), x, y, mass);
-            step.setTexture('woodBridgeStep');
-            this.world.addBody(step);
+        //     const step = new Body(new CircleShape(15), x, y, mass);
+        //     step.setTexture('woodBridgeStep');
+        //     this.world.addBody(step);
 
-            // Connect previous link to this link
-            const joint = new JointConstraint(last, step, step.position);
-            this.world.addConstraint(joint);
+        //     // Connect previous link to this link
+        //     const joint = new JointConstraint(last, step, step.position);
+        //     this.world.addConstraint(joint);
 
-            last = step;
-        }
+        //     last = step;
+        // }
 
-        // Final anchor
-        const endStep = new Body(new BoxShape(80, 20), last.position.x + 60, last.position.y - 20, 0.0);
-        endStep.setTexture('rockBridgeAnchor');
-        this.world.addBody(endStep);
+        // // Final anchor
+        // const endStep = new Body(new BoxShape(80, 20), last.position.x + 60, last.position.y - 20, 0.0);
+        // endStep.setTexture('rockBridgeAnchor');
+        // this.world.addBody(endStep);
 
-        // Connect last step to final anchor
-        const finalJoint = new JointConstraint(last, endStep, endStep.position);
-        this.world.addConstraint(finalJoint);
+        // // Connect last step to final anchor
+        // const finalJoint = new JointConstraint(last, endStep, endStep.position);
+        // this.world.addConstraint(finalJoint);
 
-        // Add pigs
-        const pig1 = new Body(new CircleShape(30), plank1.position.x + 80, floor.position.y - 50, 3.0);
-        const pig2 = new Body(new CircleShape(30), plank2.position.x + 400, floor.position.y - 50, 3.0);
-        const pig3 = new Body(new CircleShape(30), plank2.position.x + 460, floor.position.y - 50, 3.0);
-        const pig4 = new Body(new CircleShape(30), 220, 130, 1.0);
-        pig1.setTexture('pig1');
-        pig2.setTexture('pig2');
-        pig3.setTexture('pig1');
-        pig4.setTexture('pig2');
-        this.world.addBody(pig1);
-        this.world.addBody(pig2);
-        this.world.addBody(pig3);
-        this.world.addBody(pig4);
+        // // Add pigs
+        // const pig1 = new Body(new CircleShape(30), plank1.position.x + 80, floor.position.y - 50, 3.0);
+        // const pig2 = new Body(new CircleShape(30), plank2.position.x + 400, floor.position.y - 50, 3.0);
+        // const pig3 = new Body(new CircleShape(30), plank2.position.x + 460, floor.position.y - 50, 3.0);
+        // const pig4 = new Body(new CircleShape(30), 220, 130, 1.0);
+        // pig1.setTexture('pig1');
+        // pig2.setTexture('pig2');
+        // pig3.setTexture('pig1');
+        // pig4.setTexture('pig2');
+        // this.world.addBody(pig1);
+        // this.world.addBody(pig2);
+        // this.world.addBody(pig3);
+        // this.world.addBody(pig4);
+
+        const triangleVertices1 = [new Vec2(30, 30), new Vec2(-30, 30), new Vec2(0, -30)];
+        const triangle1 = new Body(new PolygonShape(triangleVertices1), Graphics.width() / 2, Graphics.height() / 2, 0);
+        this.world.addBody(triangle1);
+
+        const triangleVertices2 = [new Vec2(30, 30), new Vec2(-30, 30), new Vec2(0, -30)];
+        const triangle2 = new Body(new PolygonShape(triangleVertices2), Graphics.width() / 2, Graphics.height() / 2, 0);
+        this.world.addBody(triangle2);
     };
 
     input = (): void => {
@@ -255,6 +263,11 @@ export default class Application {
             if (!inputEvent) {
                 return;
             }
+
+            // Test for body collision
+            this.world.getBodies()[1].position.x = inputEvent.x;
+            this.world.getBodies()[1].position.y = inputEvent.y;
+            this.world.getBodies()[1].shape.updateVertices(0, this.world.getBodies()[1].position);
         }
 
         // Handle mouse click events
