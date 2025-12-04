8718df7bb5f31157a9f460e9abdc5152d3523bf8
Samuel Dalvai <s.dalvai@ewo.com>
2025-12-04 08:15:33 +0100

    test: add unit tests for collision detection between polygon and circle

--- __tests__/physics/CollisionDetection.test.ts
+++ __tests__/physics/CollisionDetection.test.ts
@@ -97,4 +97,60 @@ describe('CollisionDetection', () => {
         expect(collisionResult.isColliding).toBe(false);
         expect(collisionResult.contacts).toBeUndefined();
     });
+
+    test('detectCollisionPolygonPolygon() detects collision between fully overlapped box and circle', () => {
+        const a = new Body(new BoxShape(60, 60), 0, 0, 1.0);
+        const b = new Body(new CircleShape(30), 0, 0, 1.0);
+
+        const collisionResult = CollisionDetection.detectCollisionPolygonCircle(a, b);
+
+        expect(collisionResult.isColliding).toBe(true);
+        expect(collisionResult.contacts).not.toBeUndefined();
+        expect(collisionResult.contacts).toHaveLength(1);
+        expect(collisionResult.contacts![0].depth).toBe(60);
+    });
+
+    test('detectCollisionPolygonPolygon() detects collision between half overlapped box and circle', () => {
+        const a = new Body(new BoxShape(60, 60), 0, 0, 1.0);
+        const b = new Body(new CircleShape(30), 30, 0, 1.0);
+
+        const collisionResult = CollisionDetection.detectCollisionPolygonCircle(a, b);
+
+        expect(collisionResult.isColliding).toBe(true);
+        expect(collisionResult.contacts).not.toBeUndefined();
+        expect(collisionResult.contacts).toHaveLength(1);
+        expect(collisionResult.contacts![0].depth).toBe(30);
+    });
+
+    test('detectCollisionPolygonPolygon() detects collision between box and circle that overlap by a quarter', () => {
+        const a = new Body(new BoxShape(60, 60), 0, 0, 1.0);
+        const b = new Body(new CircleShape(30), 30, 30, 1.0);
+
+        const collisionResult = CollisionDetection.detectCollisionPolygonCircle(a, b);
+
+        expect(collisionResult.isColliding).toBe(true);
+        expect(collisionResult.contacts).not.toBeUndefined();
+        expect(collisionResult.contacts).toHaveLength(1);
+        expect(collisionResult.contacts![0].depth).toBe(30);
+    });
+
+    test('detectCollisionPolygonPolygon() should not detect collision for not overlapped box and circle', () => {
+        const a = new Body(new BoxShape(60, 60), 0, 0, 1.0);
+        const b = new Body(new CircleShape(30), 200, 200, 1.0);
+
+        const collisionResult = CollisionDetection.detectCollisionPolygonCircle(a, b);
+
+        expect(collisionResult.isColliding).toBe(false);
+        expect(collisionResult.contacts).toBeUndefined();
+    });
+
+    test('detectCollisionPolygonPolygon() should not detect collision for not overlapped and touching box and circle', () => {
+        const a = new Body(new BoxShape(60, 60), 0, 0, 1.0);
+        const b = new Body(new CircleShape(30), 60, 60, 1.0);
+
+        const collisionResult = CollisionDetection.detectCollisionPolygonCircle(a, b);
+
+        expect(collisionResult.isColliding).toBe(false);
+        expect(collisionResult.contacts).toBeUndefined();
+    });
 });
