8089a0c1f8fb01dc7e5d53ad272378204cff9758
Samuel Dalvai <samuel.dalvai@gmail.com>
2025-12-18 22:00:00 +0100

    refactor: add typescript types for texture strings

--- src/AssetStore.ts
+++ src/AssetStore.ts
@@ -1,3 +1,32 @@
+export const TEXTURES = {
+    // Base sprites
+    basketball: 'assets/basketball.png',
+    bowlingball: 'assets/bowlingball.png',
+    crate: 'assets/crate.png',
+    metal: 'assets/metal.png',
+    // Skeleton ragdoll
+    bob: 'assets/ragdoll/bob.png',
+    head: 'assets/ragdoll/head.png',
+    leftArm: 'assets/ragdoll/leftArm.png',
+    leftLeg: 'assets/ragdoll/leftLeg.png',
+    rightArm: 'assets/ragdoll/rightArm.png',
+    rightLeg: 'assets/ragdoll/rightLeg.png',
+    torso: 'assets/ragdoll/torso.png',
+    // Angry birds sprites
+    background: 'assets/angrybirds/background.png',
+    birdRed: 'assets/angrybirds/bird-red.png',
+    pig1: 'assets/angrybirds/pig-1.png',
+    pig2: 'assets/angrybirds/pig-2.png',
+    rockBox: 'assets/angrybirds/rock-box.png',
+    rockBridgeAnchor: 'assets/angrybirds/rock-bridge-anchor.png',
+    rockRound: 'assets/angrybirds/rock-round.png',
+    woodBox: 'assets/angrybirds/wood-box.png',
+    woodBridgeStep: 'assets/angrybirds/wood-bridge-step.png',
+    woodPlankCracked: 'assets/angrybirds/wood-plank-cracked.png',
+    woodPlankSolid: 'assets/angrybirds/wood-plank-solid.png',
+    woodTriangle: 'assets/angrybirds/wood-triangle.png',
+};
+
 type TextureMap = Record<string, ImageBitmap>;
 
 export default class AssetStore {
@@ -8,7 +37,7 @@ export default class AssetStore {
      * @param name The key to reference the texture
      * @param src Path to the PNG file (relative to your project)
      */
-    static async loadTexture(name: string, src: string): Promise<void> {
+    static async loadTexture(name: keyof typeof TEXTURES, src: string): Promise<void> {
         const img = new Image();
         img.src = src;
         await img.decode();
@@ -31,8 +60,10 @@ export default class AssetStore {
     }
 
     /** Preload multiple textures at once */
-    static async loadTextures(textureList: Record<string, string>): Promise<void> {
-        const promises = Object.entries(textureList).map(([name, path]) => this.loadTexture(name, path));
+    static async loadTextures(): Promise<void> {
+        const promises = Object.entries(TEXTURES).map(([name, path]) =>
+            this.loadTexture(name as keyof typeof TEXTURES, path),
+        );
         await Promise.all(promises);
     }
 }
