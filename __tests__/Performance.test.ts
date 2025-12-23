5d716fc7007c12190a17e5510214d808dfcd2c9e
Samuel Dalvai <samuel.dalvai@gmail.com>
2025-12-23 08:22:26 +0100

    refactor: update updateVertices method to use scalar math only

--- __tests__/Performance.test.ts
+++ __tests__/Performance.test.ts
@@ -1,16 +1,16 @@
 import Vec2 from '../src/math/Vec2';
 import Body from '../src/physics/Body';
-import { CircleShape } from '../src/physics/Shape';
+import { BoxShape, CircleShape } from '../src/physics/Shape';
 
 describe('Performance', () => {
     test('Performance test', () => {
         console.time('test');
-        const a = new Body(new CircleShape(20), 100, 100, 5);
+        const a = new Body(new BoxShape(20, 20), 100, 100, 5);
 
         const dt = 1 / 60;
 
-        for (let i = 0; i < 10000000; i++) {
-            a.integrateVelocities(dt);
+        for (let i = 0; i < 1000000; i++) {
+            a.shape.updateVertices(45, new Vec2(i, i));
         }
 
         console.timeEnd('test');
