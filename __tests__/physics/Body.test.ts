e94b97af556c61603f9056d5fe3a937c4749ff7a
Samuel Dalvai <samuel.dalvai@gmail.com>
2025-12-23 08:34:35 +0100

    feat: add unit tests on body local to world space convertion

--- __tests__/physics/Body.test.ts
+++ __tests__/physics/Body.test.ts
@@ -1,3 +1,4 @@
+import Vec2 from '../../src/math/Vec2';
 import Body from '../../src/physics/Body';
 import { CircleShape } from '../../src/physics/Shape';
 
@@ -15,5 +16,21 @@ describe('Body', () => {
         const b = new Body(new CircleShape(10), 100, 100, 10);
 
         expect(Body.pairKey(a, b)).toBe(Body.pairKey(b, a));
+    });
+
+    test('Should convert local body point to world space', () => {
+        const a = new Body(new CircleShape(10), 100, 100, 10);
+
+        const world = a.localSpaceToWorldSpace(new Vec2(10, 10));
+        expect(world.x).toBe(110);
+        expect(world.y).toBe(110);
+    });
+
+    test('Should convert world point to body local space', () => {
+        const a = new Body(new CircleShape(10), 100, 100, 10);
+
+        const local = a.worldSpaceToLocalSpace(new Vec2(110, 110));
+        expect(local.x).toBe(10);
+        expect(local.y).toBe(10);
     });
 });
