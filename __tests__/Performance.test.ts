2561b56a872ece3d57f287937be5033ab060c33e
Samuel Dalvai <samuel.dalvai@gmail.com>
2026-01-07 13:55:26 +0100

    test: add peformance test for joint constraint

--- __tests__/Performance.test.ts
+++ __tests__/Performance.test.ts
@@ -6,31 +6,37 @@ import { BoxShape, CircleShape } from '../src/physics/Shape';
 
 describe('Performance', () => {
     test.each(Array.from({ length: 5 }))('Performance test', () => {
-    // test('Performance test', () => {
+        // test('Performance test', () => {
         console.time('test');
         // const a = new Body(new BoxShape(20, 20), 100, 100, 5);
         // const a = new Body(new CircleShape(10), 100, 100, 10);
 
         const a = new Body(new CircleShape(60), 100, 100, 5);
-        const b = new Body(new CircleShape(60), 217.5, 100, 5);
+        const b = new Body(new CircleShape(60), 100, 200, 5);
+        const joint = new JointConstraint(a, b, new Vec2(100, 150));
+        a.position.sub(new Vec2(-100, 0));
+        b.position.add(new Vec2(100, 0));
 
         // Move bodies apart
         const numFrames = 60;
         const solverIterations = 10;
 
-        const contacts: ContactConstraint[] = [];
-        CollisionDetection.detectCollisionCircleCircle(a, b, contacts);
-        const contact = contacts![0];
+        // const contacts: ContactConstraint[] = [];
+        // CollisionDetection.detectCollisionCircleCircle(a, b, contacts);
+        // const contact = contacts![0];
 
         const deltaTime = 1 / 60;
+        const inverseDeltaTime = 1 / deltaTime;
 
         // Baseline = 850 ms
         for (let i = 0; i < 1000; i++) {
             for (let i = 0; i < numFrames; i++) {
-                contact.preSolve(1 / deltaTime);
+                joint.preSolve(inverseDeltaTime);
+                //contact.preSolve(inverseDeltaTime);
 
                 for (let j = 0; j < solverIterations; j++) {
-                    contact.solve();
+                    //contact.solve();
+                    joint.solve();
                 }
             }
         }
