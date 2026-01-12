20dd31f71840ecbebcd7a25c30b683a113cfeb77
Samuel Dalvai <samuel.dalvai@gmail.com>
2026-01-12 10:47:18 +0100

    test: enable perf test on contacts

--- __tests__/Performance.test.ts
+++ __tests__/Performance.test.ts
@@ -5,92 +5,92 @@ import { ContactConstraint, JointConstraint } from '../src/physics/Constraint';
 import { BoxShape, CircleShape } from '../src/physics/Shape';
 
 describe('Performance', () => {
-    // test.each(Array.from({ length: 5 }))('Performance test', () => {
-    //     // test('Performance test', () => {
-    //     console.time('test');
-    //     const a = new Body(new BoxShape(20, 20), 100, 100, 5);
-    //     const b = new Body(new CircleShape(10), 100, 100, 10);
-
-    //     // const a = new Body(new CircleShape(60), 100, 100, 5);
-    //     // const b = new Body(new CircleShape(60), 100, 200, 5);
-    //     // const joint = new JointConstraint(a, b, new Vec2(100, 150));
-    //     // a.position.x -= 100;
-    //     // b.position.x += 100;
-
-    //     // Move bodies apart
-    //     const numFrames = 60;
-    //     const solverIterations = 10;
-
-    //     const contacts: ContactConstraint[] = [];
-    //     CollisionDetection.detectCollisionCircleCircle(a, b, contacts);
-    //     const contact = contacts![0];
-
-    //     const deltaTime = 1 / 60;
-    //     const inverseDeltaTime = 1 / deltaTime;
-
-    //     // Baseline = 220 ms
-    //     for (let i = 0; i < 1000; i++) {
-    //         for (let i = 0; i < numFrames; i++) {
-    //             // joint.preSolve(inverseDeltaTime);
-    //             contact.preSolve(inverseDeltaTime);
-
-    //             for (let j = 0; j < solverIterations; j++) {
-    //                 contact.solve();
-    //                 // joint.solve();
-    //             }
-    //             contact.postSolve();
-    //             // joint.postSolve();
-    //         }
-    //     }
-
-    //     a.integrateVelocities(deltaTime);
-    //     b.integrateVelocities(deltaTime);
-
-    //     console.timeEnd('test');
-    // });
-
     test.each(Array.from({ length: 5 }))('Performance test', () => {
-        const boxA = new Body(new BoxShape(20, 20), 100, 100, 5);
-        const boxB = new Body(new BoxShape(20, 20), 110, 100, 5);
-        const circleA = new Body(new CircleShape(10), 100, 100, 5);
-        const circleB = new Body(new CircleShape(10), 105, 100, 5);
+        // test('Performance test', () => {
+        console.time('test');
+        const a = new Body(new BoxShape(20, 20), 100, 100, 5);
+        const b = new Body(new CircleShape(10), 100, 100, 10);
 
+        // const a = new Body(new CircleShape(60), 100, 100, 5);
+        // const b = new Body(new CircleShape(60), 100, 200, 5);
+        // const joint = new JointConstraint(a, b, new Vec2(100, 150));
+        // a.position.x -= 100;
+        // b.position.x += 100;
+
+        // Move bodies apart
+        const numFrames = 60;
+        const solverIterations = 10;
 
         const contacts: ContactConstraint[] = [];
+        CollisionDetection.detectCollisionCircleCircle(a, b, contacts);
+        const contact = contacts![0];
 
-        // circle-circle: 121 ms
-        // console.time('circle-circle');
-        // for (let i = 0; i < 10000; i++) {
-        //     CollisionDetection.detectCollisionCircleCircle(circleA, circleB, contacts);
-        // }
-        // console.timeEnd('circle-circle');
+        const deltaTime = 1 / 60;
+        const inverseDeltaTime = 1 / deltaTime;
 
-        // // polygon-polygon: 287 ms
-        // contacts.length = 0;
-        // console.time('polygon-polygon');
-        // for (let i = 0; i < 10000; i++) {
-        //     CollisionDetection.detectCollisionPolygonPolygon(boxA, boxB, contacts);
-        // }
-        // console.timeEnd('polygon-polygon');
+        // Baseline = 220 ms
+        for (let i = 0; i < 1000; i++) {
+            for (let i = 0; i < numFrames; i++) {
+                // joint.preSolve(inverseDeltaTime);
+                contact.preSolve(inverseDeltaTime);
 
-        // // polygon-circle: 136 ms
-        // contacts.length = 0;
-        // console.time('polygon-circle');
-        // for (let i = 0; i < 10000; i++) {
-        //     CollisionDetection.detectCollisionPolygonCircle(boxA, circleA, contacts);
-        // }
-        // console.timeEnd('polygon-circle');
-
-        // polygon-circle: 537 ms
-        contacts.length = 0;
-        console.time('mixed-shape');
-        for (let i = 0; i < 10000; i++) {
-            CollisionDetection.detectCollision(boxA, boxB, contacts);
-            CollisionDetection.detectCollision(circleA, circleB, contacts);
-            CollisionDetection.detectCollision(boxA, circleA, contacts);
+                for (let j = 0; j < solverIterations; j++) {
+                    contact.solve();
+                    // joint.solve();
+                }
+                contact.postSolve();
+                // joint.postSolve();
+            }
         }
-        console.timeEnd('mixed-shape');
+
+        a.integrateVelocities(deltaTime);
+        b.integrateVelocities(deltaTime);
+
+        console.timeEnd('test');
     });
+
+    // test.each(Array.from({ length: 5 }))('Performance test', () => {
+    //     const boxA = new Body(new BoxShape(20, 20), 100, 100, 5);
+    //     const boxB = new Body(new BoxShape(20, 20), 110, 100, 5);
+    //     const circleA = new Body(new CircleShape(10), 100, 100, 5);
+    //     const circleB = new Body(new CircleShape(10), 105, 100, 5);
+
+
+    //     const contacts: ContactConstraint[] = [];
+
+    //     // circle-circle: 121 ms
+    //     // console.time('circle-circle');
+    //     // for (let i = 0; i < 10000; i++) {
+    //     //     CollisionDetection.detectCollisionCircleCircle(circleA, circleB, contacts);
+    //     // }
+    //     // console.timeEnd('circle-circle');
+
+    //     // // polygon-polygon: 287 ms
+    //     // contacts.length = 0;
+    //     // console.time('polygon-polygon');
+    //     // for (let i = 0; i < 10000; i++) {
+    //     //     CollisionDetection.detectCollisionPolygonPolygon(boxA, boxB, contacts);
+    //     // }
+    //     // console.timeEnd('polygon-polygon');
+
+    //     // // polygon-circle: 136 ms
+    //     // contacts.length = 0;
+    //     // console.time('polygon-circle');
+    //     // for (let i = 0; i < 10000; i++) {
+    //     //     CollisionDetection.detectCollisionPolygonCircle(boxA, circleA, contacts);
+    //     // }
+    //     // console.timeEnd('polygon-circle');
+
+    //     // polygon-circle: 537 ms
+    //     contacts.length = 0;
+    //     console.time('mixed-shape');
+    //     for (let i = 0; i < 10000; i++) {
+    //         CollisionDetection.detectCollision(boxA, boxB, contacts);
+    //         CollisionDetection.detectCollision(circleA, circleB, contacts);
+    //         CollisionDetection.detectCollision(boxA, circleA, contacts);
+    //     }
+    //     console.timeEnd('mixed-shape');
+    // });
 
     // test('Performance test', () => {
     //     const a = new Body(new BoxShape(20, 20), 100, 100, 5);
