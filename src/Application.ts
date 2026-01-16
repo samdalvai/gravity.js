ff330fda9a6f5dec9a244f3137abecd9d1840044
Samuel Dalvai <samuel.dalvai@gmail.com>
2026-01-16 07:31:31 +0100

    feat: disable current world and application implementation

--- src/Application.ts
+++ src/Application.ts
@@ -14,11 +14,11 @@ export default class Application {
     private running = false;
     private world: World;
     private bgTexture: ImageBitmap | null = null;
-    private generateParticle = false;
-    private generateCircles = true;
-    private showContacts = true;
-    private demoIndex = 1;
-    private bomb: Body | null = null;
+    // private generateParticle = false;
+    // private generateCircles = true;
+    // private showContacts = true;
+    // private demoIndex = 1;
+    // private bomb: Body | null = null;
 
     // Debug related properties
     private debug = true;
@@ -43,7 +43,7 @@ export default class Application {
         await AssetStore.loadTextures();
 
         this.running = Graphics.openWindow();
-        Demo.demo1(this.world);
+        // Demo.demo1(this.world);
 
         this.bgTexture = AssetStore.getTexture('background');
     };
@@ -69,69 +69,69 @@ export default class Application {
                             const radius = 250; // pixels
                             const strength = 10000; // you can tune this
 
-                            for (const body of this.world.getBodies()) {
-                                const explosionImpulse = Force.generateExplosionForce(
-                                    body,
-                                    explosionPos,
-                                    radius,
-                                    strength,
-                                );
-                                body.applyImpulseLinear(explosionImpulse);
-                            }
+                            // for (const body of this.world.getBodies()) {
+                            //     const explosionImpulse = Force.generateExplosionForce(
+                            //         body,
+                            //         explosionPos,
+                            //         radius,
+                            //         strength,
+                            //     );
+                            //     body.applyImpulseLinear(explosionImpulse);
+                            // }
                         }
                     }
 
                     if (inputEvent.code === 'Space') {
                         // Drop bomb
-                        if (!this.bomb) {
-                            const bomb = new Body(new CircleShape(30), Graphics.width() / 2, 0, 10);
-                            bomb.friction = 0.2;
-                            this.bomb = bomb;
-                            this.bomb.setTexture('rockRound');
+                        // if (!this.bomb) {
+                        //     const bomb = new Body(new CircleShape(30), Graphics.width() / 2, 0, 10);
+                        //     bomb.friction = 0.2;
+                        //     this.bomb = bomb;
+                        //     this.bomb.setTexture('rockRound');
 
-                            this.world.addBody(bomb);
-                        }
+                        //     this.world.addBody(bomb);
+                        // }
 
-                        this.bomb.position = new Vec2(Graphics.width() / 2 + Utils.randomNumber(-500, 500), -50);
-                        const middleFloor = new Vec2(
-                            Graphics.width() / 2 + Utils.randomNumber(-100, 100),
-                            Graphics.height() - 300,
-                        );
-                        const vectorFromBombToFloor = middleFloor.subNew(this.bomb.position);
-                        this.bomb.velocity = vectorFromBombToFloor;
+                        // this.bomb.position = new Vec2(Graphics.width() / 2 + Utils.randomNumber(-500, 500), -50);
+                        // const middleFloor = new Vec2(
+                        //     Graphics.width() / 2 + Utils.randomNumber(-100, 100),
+                        //     Graphics.height() - 300,
+                        // );
+                        // const vectorFromBombToFloor = middleFloor.subNew(this.bomb.position);
+                        // this.bomb.velocity = vectorFromBombToFloor;
                     }
 
-                    if (inputEvent.key === 'g') {
-                        this.generateParticle = true;
-                    }
+                    // if (inputEvent.key === 'g') {
+                    //     this.generateParticle = true;
+                    // }
 
-                    if (inputEvent.key === 'c') {
-                        this.generateCircles = !this.generateCircles;
-                    }
+                    // if (inputEvent.key === 'c') {
+                    //     this.generateCircles = !this.generateCircles;
+                    // }
 
-                    if (inputEvent.key === 's') {
-                        this.showContacts = !this.showContacts;
-                    }
+                    // if (inputEvent.key === 's') {
+                    //     this.showContacts = !this.showContacts;
+                    // }
 
-                    if (!Number.isNaN(Number.parseInt(inputEvent.key))) {
-                        const index = Number.parseInt(inputEvent.key);
-                        this.demoIndex = index;
-                        const demo = Demo.demoFunctions[this.demoIndex];
+                    // if (!Number.isNaN(Number.parseInt(inputEvent.key))) {
+                    //     const index = Number.parseInt(inputEvent.key);
+                    //     this.demoIndex = index;
+                    //     const demo = Demo.demoFunctions[this.demoIndex];
 
-                        if (!demo) {
-                            throw new Error(`Demo ${index} does not exist`);
-                        }
+                    //     if (!demo) {
+                    //         throw new Error(`Demo ${index} does not exist`);
+                    //     }
 
-                        this.world.clear();
-                        this.bomb = null;
-                        demo(this.world);
-                    }
+                    //     this.world.clear();
+                    //     this.bomb = null;
+                    //     demo(this.world);
+                    // }
 
                     break;
                 case 'keyup':
-                    if (inputEvent.key === 'g') {
-                        this.generateParticle = false;
-                    }
+                    // if (inputEvent.key === 'g') {
+                    //     this.generateParticle = false;
+                    // }
 
                     break;
             }
@@ -161,27 +161,27 @@ export default class Application {
 
             switch (inputEvent.type) {
                 case 'mousedown':
-                    if (this.world.getBodies().length >= MAX_BODIES) {
-                        continue;
-                    }
+                    // if (this.world.getBodies().length >= MAX_BODIES) {
+                    //     continue;
+                    // }
 
                     switch (inputEvent.button) {
                         case MouseButton.LEFT:
                             {
-                                const ball = new Body(new CircleShape(30), inputEvent.x, inputEvent.y, 4.0);
-                                ball.restitution = 0.6;
-                                ball.friction = 0.5;
-                                ball.setTexture('basketball');
-                                this.world.addBody(ball);
+                                // const ball = new Body(new CircleShape(30), inputEvent.x, inputEvent.y, 4.0);
+                                // ball.restitution = 0.6;
+                                // ball.friction = 0.5;
+                                // ball.setTexture('basketball');
+                                // this.world.addBody(ball);
                             }
                             break;
                         case MouseButton.RIGHT:
                             {
-                                const box = new Body(new BoxShape(60, 60), inputEvent.x, inputEvent.y, 6.0);
-                                box.restitution = 0.2;
-                                box.friction = 0.7;
-                                box.setTexture('crate');
-                                this.world.addBody(box);
+                                // const box = new Body(new BoxShape(60, 60), inputEvent.x, inputEvent.y, 6.0);
+                                // box.restitution = 0.2;
+                                // box.friction = 0.7;
+                                // box.setTexture('crate');
+                                // this.world.addBody(box);
                             }
                             break;
                     }
@@ -202,20 +202,20 @@ export default class Application {
 
         this.world.update(deltaTime);
 
-        if (this.generateParticle) {
-            for (let i = 0; i < 10; i++) {
-                if (this.world.getBodies().length >= MAX_BODIES) {
-                    continue;
-                }
+        // if (this.generateParticle) {
+        //     for (let i = 0; i < 10; i++) {
+        //         if (this.world.getBodies().length >= MAX_BODIES) {
+        //             continue;
+        //         }
 
-                const shape = this.generateCircles ? new CircleShape(5) : new BoxShape(10, 10);
-                const particle = new Body(shape, InputManager.mousePosition.x, InputManager.mousePosition.y, 1.0);
-                particle.restitution = 0.0;
-                particle.friction = 10;
-                particle.setTexture(this.generateCircles ? 'rockRound' : 'rockBox');
-                this.world.addBody(particle);
-            }
-        }
+        //         const shape = this.generateCircles ? new CircleShape(5) : new BoxShape(10, 10);
+        //         const particle = new Body(shape, InputManager.mousePosition.x, InputManager.mousePosition.y, 1.0);
+        //         particle.restitution = 0.0;
+        //         particle.friction = 10;
+        //         particle.setTexture(this.generateCircles ? 'rockRound' : 'rockBox');
+        //         this.world.addBody(particle);
+        //     }
+        // }
     };
 
     render = (): void => {
@@ -234,48 +234,48 @@ export default class Application {
         }
 
         // Draw all bodies
-        for (const body of this.world.getBodies()) {
-            Graphics.drawBody(body, this.debug);
-        }
+        // for (const body of this.world.getBodies()) {
+        //     Graphics.drawBody(body, this.debug);
+        // }
 
         // Draw all joints anchor points and debug properties
         if (this.debug) {
-            if (this.showContacts) {
-                for (const joint of this.world.getJoints()) {
-                    // TODO: this is just a simple draw method, we need to consider local anchors rather than just
-                    // bodies position
-                    Graphics.drawLine(
-                        joint.bodyA.position.x,
-                        joint.bodyA.position.y,
-                        joint.bodyB.position.x,
-                        joint.bodyB.position.y,
-                        'blue',
-                    );
-                }
+            // if (this.showContacts) {
+            //     for (const joint of this.world.getJoints()) {
+            //         // TODO: this is just a simple draw method, we need to consider local anchors rather than just
+            //         // bodies position
+            //         Graphics.drawLine(
+            //             joint.bodyA.position.x,
+            //             joint.bodyA.position.y,
+            //             joint.bodyB.position.x,
+            //             joint.bodyB.position.y,
+            //             'blue',
+            //         );
+            //     }
 
-                for (const contact of this.world.getContacts()) {
-                    const aW = contact.bodyA.localSpaceToWorldSpace(contact.aPointLocal);
-                    const bW = contact.bodyB.localSpaceToWorldSpace(contact.bPointLocal);
-                    // const aW = contact.start;
-                    // const bW = contact.end;
-                    Graphics.drawFillCircle(aW.x, aW.y, 5, 'red');
-                    Graphics.drawFillCircle(bW.x, bW.y, 2, 'red');
-                    Graphics.drawLine(aW.x, aW.y, bW.x, bW.y, 'red');
-                }
-            }
+            //     for (const contact of this.world.getContacts()) {
+            //         const aW = contact.bodyA.localSpaceToWorldSpace(contact.aPointLocal);
+            //         const bW = contact.bodyB.localSpaceToWorldSpace(contact.bPointLocal);
+            //         // const aW = contact.start;
+            //         // const bW = contact.end;
+            //         Graphics.drawFillCircle(aW.x, aW.y, 5, 'red');
+            //         Graphics.drawFillCircle(bW.x, bW.y, 2, 'red');
+            //         Graphics.drawLine(aW.x, aW.y, bW.x, bW.y, 'red');
+            //     }
+            // }
         }
 
         const debugText = [
             // General info
             'Keys: 1-9 Demos, Left Mouse to generate circles, Right Mouse to generate boxes, Space to drop bomb',
-            `${Demo.demoStrings[this.demoIndex]}`,
-            `(D)ebug mode: ${this.debug ? 'ON' : 'OFF'}`,
-            `(C)hosen particle: ${this.generateCircles ? 'Circle' : 'Box'}`,
-            `(S)how contacts and joints: ${this.showContacts ? 'ON' : 'OFF'}`,
-            // Debut related info
+            // `${Demo.demoStrings[this.demoIndex]}`,
+            // `(D)ebug mode: ${this.debug ? 'ON' : 'OFF'}`,
+            // `(C)hosen particle: ${this.generateCircles ? 'Circle' : 'Box'}`,
+            // `(S)how contacts and joints: ${this.showContacts ? 'ON' : 'OFF'}`,
+            // // Debut related info
             `FPS: ${this.FPS.toFixed(2)}`,
-            `Num objects: ${this.world.getBodies().length}`,
-            `Num contacts: ${this.world.getContacts().length}`,
+            // `Num objects: ${this.world.getBodies().length}`,
+            // `Num contacts: ${this.world.getContacts().length}`,
         ];
 
         for (let i = 0; i < debugText.length - (this.debug ? 0 : 4); i++) {
