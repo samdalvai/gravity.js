51c661fe333a7d5d776178d14c8560ecb488a5a8
Samuel Dalvai <samuel.dalvai@gmail.com>
2026-01-17 22:00:00 +0100

    feat: add test for local to world space vertices convertion

--- __tests__/adaptation/detection.test.ts
+++ __tests__/adaptation/detection.test.ts
@@ -7,17 +7,24 @@ import Body from '../../src/physics/Body';
 import { BoxShape, PolygonShape } from '../../src/physics/Shape';
 
 describe('Performance', () => {
-    test('Test', () => {
+    test('Test convertion from local to world space for vertices', () => {
         const b1 = new Box(50);
         b1.mass = 2.0;
-        b1.position = new Vector2(0, 0);
+        b1.position = new Vector2(100, 100);
         b1.restitution = 0.2;
 
         console.log(b1.vertices);
+        const worldVertices = [];
+        for (const v of b1.vertices) {
+            const worldV = b1.localToGlobal.mulVector2(v, 1);
+            worldVertices.push(worldV);
+        }
+        console.log(worldVertices);
 
-        const b2 = new Body(new BoxShape(50, 50), 0, 0, 2.0);
+        const b2 = new Body(new BoxShape(50, 50), 100, 100, 2.0);
         const shape = b2.shape as PolygonShape;
 
         console.log(shape.localVertices);
+        console.log(shape.worldVertices);
     });
 });
