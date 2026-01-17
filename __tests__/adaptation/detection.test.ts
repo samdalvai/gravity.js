572c9190a2bbd7ec30d6b125bda5e895ebb8921d
Samuel Dalvai <samuel.dalvai@gmail.com>
2026-01-17 22:00:00 +0100

    feat: add blueprint test for adaptation

---
+++ __tests__/adaptation/detection.test.ts
@@ -0,0 +1,8 @@
+import { support } from '../../src/new/detection'
+import { support_adapted } from '../../src/new/detection_adapted'
+
+describe('Performance', () => {
+    test('Test', () => {
+        
+    });
+});
