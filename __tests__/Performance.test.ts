d5a3465f12c9c08170d8ddd38ea15cd9cfe1ddf8
Samuel Dalvai <samuel.dalvai@gmail.com>
2026-01-17 22:00:00 +0100

    test: log vertices

--- __tests__/Performance.test.ts
+++ __tests__/Performance.test.ts
@@ -1,60 +1,60 @@
 import Vec2 from '../src/math/Vec2';
+import { assert } from '../src/new/util';
 import Body from '../src/physics/Body';
 import CollisionDetection from '../src/physics/CollisionDetection';
 import { ContactConstraint, JointConstraint } from '../src/physics/Constraint';
 import { BoxShape, CircleShape } from '../src/physics/Shape';
 
 describe('Performance', () => {
-    test.each(Array.from({ length: 5 }))('Performance test', () => {
-        // test('Performance test', () => {
-        console.time('test');
-        const a = new Body(new BoxShape(20, 20), 100, 100, 5);
-        const b = new Body(new CircleShape(10), 100, 100, 10);
+    // test.each(Array.from({ length: 5 }))('Performance test', () => {
+    //     // test('Performance test', () => {
+    //     console.time('test');
+    //     const a = new Body(new BoxShape(20, 20), 100, 100, 5);
+    //     const b = new Body(new CircleShape(10), 100, 100, 10);
 
-        // const a = new Body(new CircleShape(60), 100, 100, 5);
-        // const b = new Body(new CircleShape(60), 100, 200, 5);
-        // const joint = new JointConstraint(a, b, new Vec2(100, 150));
-        // a.position.x -= 100;
-        // b.position.x += 100;
+    //     // const a = new Body(new CircleShape(60), 100, 100, 5);
+    //     // const b = new Body(new CircleShape(60), 100, 200, 5);
+    //     // const joint = new JointConstraint(a, b, new Vec2(100, 150));
+    //     // a.position.x -= 100;
+    //     // b.position.x += 100;
 
-        // Move bodies apart
-        const numFrames = 60;
-        const solverIterations = 10;
+    //     // Move bodies apart
+    //     const numFrames = 60;
+    //     const solverIterations = 10;
 
-        const contacts: ContactConstraint[] = [];
-        CollisionDetection.detectCollisionCircleCircle(a, b, contacts);
-        const contact = contacts![0];
+    //     const contacts: ContactConstraint[] = [];
+    //     CollisionDetection.detectCollisionCircleCircle(a, b, contacts);
+    //     const contact = contacts![0];
 
-        const deltaTime = 1 / 60;
-        const inverseDeltaTime = 1 / deltaTime;
+    //     const deltaTime = 1 / 60;
+    //     const inverseDeltaTime = 1 / deltaTime;
 
-        // Baseline = 220 ms
-        for (let i = 0; i < 1000; i++) {
-            for (let i = 0; i < numFrames; i++) {
-                // joint.preSolve(inverseDeltaTime);
-                contact.preSolve(inverseDeltaTime);
+    //     // Baseline = 220 ms
+    //     for (let i = 0; i < 1000; i++) {
+    //         for (let i = 0; i < numFrames; i++) {
+    //             // joint.preSolve(inverseDeltaTime);
+    //             contact.preSolve(inverseDeltaTime);
 
-                for (let j = 0; j < solverIterations; j++) {
-                    contact.solve();
-                    // joint.solve();
-                }
-                contact.postSolve();
-                // joint.postSolve();
-            }
-        }
+    //             for (let j = 0; j < solverIterations; j++) {
+    //                 contact.solve();
+    //                 // joint.solve();
+    //             }
+    //             contact.postSolve();
+    //             // joint.postSolve();
+    //         }
+    //     }
 
-        a.integrateVelocities(deltaTime);
-        b.integrateVelocities(deltaTime);
+    //     a.integrateVelocities(deltaTime);
+    //     b.integrateVelocities(deltaTime);
 
-        console.timeEnd('test');
-    });
+    //     console.timeEnd('test');
+    // });
 
     // test.each(Array.from({ length: 5 }))('Performance test', () => {
     //     const boxA = new Body(new BoxShape(20, 20), 100, 100, 5);
     //     const boxB = new Body(new BoxShape(20, 20), 110, 100, 5);
     //     const circleA = new Body(new CircleShape(10), 100, 100, 5);
     //     const circleB = new Body(new CircleShape(10), 105, 100, 5);
-
 
     //     const contacts: ContactConstraint[] = [];
 
@@ -104,4 +104,8 @@ describe('Performance', () => {
     //     }
     //     console.timeEnd('time');
     // });
+
+    test('', () => {
+        expect(true).toBe(true);
+    });
 });
