391a2a57236b00c80770db0389686c7539263594
Samuel Dalvai <samuel.dalvai@gmail.com>
2026-01-17 21:51:36 +0100

    test: log vertices

--- __tests__/adaptation/detection.test.ts
+++ __tests__/adaptation/detection.test.ts
@@ -1,8 +1,23 @@
-import { support } from '../../src/new/detection'
-import { support_adapted } from '../../src/new/detection_adapted'
+import { Box } from '../../src/new/box';
+import { support } from '../../src/new/detection';
+import { support_adapted } from '../../src/new/detection_adapted';
+import { Vector2 } from '../../src/new/math/vector2';
+import { RigidBody } from '../../src/new/rigidbody';
+import Body from '../../src/physics/Body';
+import { BoxShape, PolygonShape } from '../../src/physics/Shape';
 
 describe('Performance', () => {
     test('Test', () => {
-        
+        const b1 = new Box(50);
+        b1.mass = 2.0;
+        b1.position = new Vector2(0, 0);
+        b1.restitution = 0.2;
+
+        console.log(b1.vertices);
+
+        const b2 = new Body(new BoxShape(50, 50), 0, 0, 2.0);
+        const shape = b2.shape as PolygonShape;
+
+        console.log(shape.localVertices);
     });
 });
