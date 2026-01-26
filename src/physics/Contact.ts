7c3edfd1dea202cb19961fa07f21cd1afc809455
Samuel Dalvai <samuel.dalvai@gmail.com>
2026-01-26 14:28:15 +0100

    docs: add notive for the project

--- src/physics/Contact.ts
+++ src/physics/Contact.ts
@@ -1,3 +1,12 @@
+/*
+ * Portions of this file are derived from the Sopiro Physics Engine.
+ *
+ * Copyright (c) 2022 Sopiro
+ * Licensed under the MIT License
+ *
+ * Original project:
+ * https://github.com/Sopiro
+ */
 import { Mat2 } from '../math/Mat2';
 import Utils from '../math/Utils';
 import Vec2 from '../math/Vec2';
