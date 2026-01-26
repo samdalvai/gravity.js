75c935716daa89806f6a0e73ea0cf9b0163ec941
Samuel Dalvai <samuel.dalvai@gmail.com>
2026-01-26 16:20:01 +0100

    refactor: update impulse method for solver

--- __tests__/Performance.test.ts
+++ __tests__/Performance.test.ts
@@ -1,52 +1,47 @@
 import Vec2 from '../src/math/Vec2';
-import RigidBody from '../src/physics/RigidBody';
 import CollisionDetection from '../src/physics/CollisionDetection';
+import RigidBody from '../src/physics/RigidBody';
 import { BoxShape, CircleShape } from '../src/physics/Shape';
 
 describe('Performance', () => {
-    // test.each(Array.from({ length: 5 }))('Performance test', () => {
-    //     // test('Performance test', () => {
-    //     console.time('test');
-    //     const a = new RigidBody(new BoxShape(20, 20), 100, 100, 5);
-    //     const b = new RigidBody(new CircleShape(10), 100, 100, 10);
+    test.each(Array.from({ length: 5 }))('Performance test', () => {
+        // test('Performance test', () => {
+        console.time('test');
+        const a = new RigidBody(new BoxShape(20, 20), 100, 100, 5);
+        // const b = new RigidBody(new CircleShape(10), 100, 100, 10);
+        const b = new RigidBody(new BoxShape(20, 20), 110, 100, 10);
 
-    //     // const a = new RigidBody(new CircleShape(60), 100, 100, 5);
-    //     // const b = new RigidBody(new CircleShape(60), 100, 200, 5);
-    //     // const joint = new JointConstraint(a, b, new Vec2(100, 150));
-    //     // a.position.x -= 100;
-    //     // b.position.x += 100;
+        // const a = new RigidBody(new CircleShape(60), 100, 100, 5);
+        // const b = new RigidBody(new CircleShape(60), 100, 200, 5);
+        // const joint = new JointConstraint(a, b, new Vec2(100, 150));
+        // a.position.x -= 100;
+        // b.position.x += 100;
 
-    //     // Move bodies apart
-    //     const numFrames = 60;
-    //     const solverIterations = 10;
+        // Move bodies apart
+        const numFrames = 60;
+        const solverIterations = 10;
 
-    //     const contacts: ContactConstraint[] = [];
-    //     CollisionDetection.detectCollisionCircleCircle(a, b, contacts);
-    //     const contact = contacts![0];
+        const contact = CollisionDetection.detectCollision(a, b)!;
 
-    //     const deltaTime = 1 / 60;
-    //     const inverseDeltaTime = 1 / deltaTime;
+        const deltaTime = 1 / 60;
+        const inverseDeltaTime = 1 / deltaTime;
 
-    //     // Baseline = 220 ms
-    //     for (let i = 0; i < 1000; i++) {
-    //         for (let i = 0; i < numFrames; i++) {
-    //             // joint.preSolve(inverseDeltaTime);
-    //             contact.preSolve(inverseDeltaTime);
+        // Baseline = 540 ms (normal)
+        // Baseline = 680 ms (block solve)
+        for (let i = 0; i < 1000; i++) {
+            for (let i = 0; i < numFrames; i++) {
+                // joint.preSolve(inverseDeltaTime);
+                contact.preSolve(inverseDeltaTime);
 
-    //             for (let j = 0; j < solverIterations; j++) {
-    //                 contact.solve();
-    //                 // joint.solve();
-    //             }
-    //             contact.postSolve();
-    //             // joint.postSolve();
-    //         }
-    //     }
+                for (let j = 0; j < solverIterations; j++) {
+                    contact.solve();
+                    // joint.solve();
+                }
+            }
+        }
 
-    //     a.integrateVelocities(deltaTime);
-    //     b.integrateVelocities(deltaTime);
-
-    //     console.timeEnd('test');
-    // });
+        console.timeEnd('test');
+    });
 
     // test.each(Array.from({ length: 5 }))('Performance test', () => {
     //     const boxA = new RigidBody(new BoxShape(20, 20), 100, 100, 5);
