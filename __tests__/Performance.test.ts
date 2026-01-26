4fe8d7b41937027bd364977049fbcccfe45d356c
Samuel Dalvai <samuel.dalvai@gmail.com>
2026-01-26 22:00:00 +0100

    test: add peformance test on joint solver

--- __tests__/Performance.test.ts
+++ __tests__/Performance.test.ts
@@ -1,5 +1,6 @@
 import Vec2 from '../src/math/Vec2';
 import CollisionDetection from '../src/physics/CollisionDetection';
+import { DistanceJoint } from '../src/physics/DistanceJoint';
 import RigidBody from '../src/physics/RigidBody';
 import { BoxShape, CircleShape } from '../src/physics/Shape';
 
@@ -8,20 +9,20 @@ describe('Performance', () => {
         // test('Performance test', () => {
         console.time('test');
         const a = new RigidBody(new BoxShape(20, 20), 100, 100, 5);
-        const b = new RigidBody(new CircleShape(10), 100, 100, 10);
-        // const b = new RigidBody(new BoxShape(20, 20), 110, 100, 10);
+        // const b = new RigidBody(new CircleShape(10), 100, 100, 10);
+        const b = new RigidBody(new BoxShape(20, 20), 110, 100, 10);
 
         // const a = new RigidBody(new CircleShape(60), 100, 100, 5);
         // const b = new RigidBody(new CircleShape(60), 100, 200, 5);
-        // const joint = new JointConstraint(a, b, new Vec2(100, 150));
-        // a.position.x -= 100;
-        // b.position.x += 100;
+        const joint = new DistanceJoint(a, b);
+        a.position.x -= 100;
+        b.position.x += 100;
 
         // Move bodies apart
         const numFrames = 60;
         const solverIterations = 10;
 
-        const contact = CollisionDetection.detectCollision(a, b)!;
+        // const contact = CollisionDetection.detectCollision(a, b)!;
 
         const deltaTime = 1 / 60;
         const inverseDeltaTime = 1 / deltaTime;
@@ -30,12 +31,12 @@ describe('Performance', () => {
         // Baseline = 680 ms (block solve)
         for (let i = 0; i < 1000; i++) {
             for (let i = 0; i < numFrames; i++) {
-                // joint.preSolve(inverseDeltaTime);
-                contact.preSolve(inverseDeltaTime);
+                joint.preSolve(inverseDeltaTime);
+                // contact.preSolve(inverseDeltaTime);
 
                 for (let j = 0; j < solverIterations; j++) {
-                    contact.solve();
-                    // joint.solve();
+                    // contact.solve();
+                    joint.solve();
                 }
             }
         }
