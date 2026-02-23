0e1613e3872356fb043de29f2e26537462cd0e98
Samuel Dalvai <samuel.dalvai@gmail.com>
2026-02-23 22:00:00 +0100

    refactor: set body removal threshold in any direction

--- src/core/World.ts
+++ src/core/World.ts
@@ -188,7 +188,10 @@ export default class World {
             const body = this.bodies[i];
 
             // It suffices to look for the position going below the screen
-            if (body.position.y < BODY_REMOVAL_THRESHOLD) {
+            if (
+                Math.abs(body.position.x) > BODY_REMOVAL_THRESHOLD ||
+                Math.abs(body.position.y) > BODY_REMOVAL_THRESHOLD
+            ) {
                 this.bodies[i] = this.bodies[this.bodies.length - 1];
                 this.bodies.pop();
             }
