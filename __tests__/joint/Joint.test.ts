b18a6184b35fe11b768e50c0067ace5184a5b2cf
Samuel Dalvai <samuel.dalvai@gmail.com>
2026-02-23 22:00:00 +0100

    test: update folders for tests

--- __tests__/joint/Joint.test.ts
+++ __tests__/joint/Joint.test.ts
@@ -1,9 +1,8 @@
-import * as Collision from '../../src/collision/Collision';
-import { DistanceJoint } from '../../src/joint/DistanceJoint';
 import RigidBody from '../../src/core/RigidBody';
+import { DistanceJoint } from '../../src/joint/DistanceJoint';
 import { CircleShape } from '../../src/shapes/CircleShape';
 
-describe('Constraint', () => {
+describe('Joint', () => {
     test('Joint constraint solving should apply impulses to correct position of bodies', () => {
         const a = new RigidBody(new CircleShape(60), 100, 100, 5);
         const b = new RigidBody(new CircleShape(60), 100, 200, 5);
@@ -38,37 +37,5 @@ describe('Constraint', () => {
 
         expect(Math.abs(b.velocity.x)).toBe(0);
         expect(Math.abs(b.velocity.y + 260)).toBe(3.9405078930655577);
-    });
-
-    test('Penetration constraint solving should apply impulses to correct position of bodies', () => {
-        const a = new RigidBody(new CircleShape(60), 100, 100, 5);
-        const b = new RigidBody(new CircleShape(60), 200, 100, 5);
-
-        // Move bodies apart
-        const numFrames = 60;
-        const solverIterations = 20;
-
-        const manifold = Collision.detectCollisionCircleCircle(a, b)!;
-
-        const deltaTime = 1 / 60;
-        for (let i = 0; i < numFrames; i++) {
-            manifold.preSolve(1 / deltaTime);
-
-            for (let j = 0; j < solverIterations; j++) {
-                manifold.solve();
-            }
-        }
-
-        a.integrateVelocities(deltaTime);
-        b.integrateVelocities(deltaTime);
-
-        expect(a.position.y).toBe(100);
-        expect(b.position.y).toBe(100);
-
-        // Check that the solver moved the objects apart
-        expect(a.position.x).toBe(98.05);
-        expect(b.position.x).toBe(201.95);
-        expect(a.velocity.x).toBe(-117);
-        expect(b.velocity.x).toBe(117);
     });
 });
