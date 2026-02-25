1872706976613f3fca9d16d365cc6e5caeb19db7
Samuel Dalvai <samuel.dalvai@gmail.com>
2026-02-25 22:00:00 +0100

    refactor: move ccd logic to a separate class

---
+++ src/collision/CCD.ts
@@ -0,0 +1,122 @@
+import RigidBody from '../core/RigidBody';
+import Vec2 from '../math/Vec2';
+import { CapsuleShape } from '../shapes/CapsuleShape';
+import { CircleShape } from '../shapes/CircleShape';
+import { edgeCircleIntersection, edgeIntersection } from '../shapes/Edge';
+import { PolygonShape } from '../shapes/PolygonShape';
+import { ShapeType } from '../shapes/Shape';
+
+export function resolveCCD(bullet: RigidBody, bodies: RigidBody[], dt: number): void {
+    const bulletShape = bullet.shape as CircleShape;
+    const currentPos = bullet.position.copy();
+    const nextPos = currentPos.addNew(bullet.velocity.scaleNew(dt));
+
+    let minDistanceSquared = Infinity;
+    let closestIntersection: Vec2 | undefined;
+
+    // TODO: We could cast two rays instead of one or check intersection by shifting up and down by radius
+    for (const other of bodies) {
+        if (bullet.id === other.id || other.isBullet) continue;
+
+        if (other.shapeType === ShapeType.BOX || other.shapeType === ShapeType.POLYGON) {
+            const polygonShape = other.shape as PolygonShape;
+            const vertices = polygonShape.worldVertices;
+
+            for (let i = 0; i < vertices.length; i++) {
+                const v0 = vertices[i];
+                const v1 = vertices[(i + 1) % vertices.length];
+
+                const intersection = edgeIntersection(currentPos, nextPos, v0, v1);
+
+                if (intersection) {
+                    const distanceSquared = intersection.subNew(currentPos).magnitudeSquared();
+
+                    if (distanceSquared < minDistanceSquared) {
+                        closestIntersection = intersection.copy();
+                        minDistanceSquared = distanceSquared;
+                    }
+                }
+            }
+        }
+
+        if (other.shapeType === ShapeType.CIRCLE) {
+            const circleShape = other.shape as CircleShape;
+            const intersections = edgeCircleIntersection(currentPos, nextPos, other.position, circleShape.radius);
+
+            for (const int of intersections) {
+                const distanceSquared = int.subNew(currentPos).magnitudeSquared();
+
+                if (distanceSquared < minDistanceSquared) {
+                    closestIntersection = int.copy();
+                    minDistanceSquared = distanceSquared;
+                }
+            }
+        }
+
+        if (other.shapeType === ShapeType.CAPSULE) {
+            const capsuleShape = other.shape as CapsuleShape;
+            const topCirclePosition = capsuleShape.getTopCirclePosition(other);
+            const bottomCirclePosition = capsuleShape.getBottomCirclePosition(other);
+
+            const topCircleIntersections = edgeCircleIntersection(
+                currentPos,
+                nextPos,
+                topCirclePosition,
+                capsuleShape.radius,
+            );
+
+            for (const int of topCircleIntersections) {
+                const distanceSquared = int.subNew(currentPos).magnitudeSquared();
+
+                if (distanceSquared < minDistanceSquared) {
+                    closestIntersection = int.copy();
+                    minDistanceSquared = distanceSquared;
+                }
+            }
+
+            const bottomCircleIntersections = edgeCircleIntersection(
+                currentPos,
+                nextPos,
+                bottomCirclePosition,
+                capsuleShape.radius,
+            );
+
+            for (const int of bottomCircleIntersections) {
+                const distanceSquared = int.subNew(currentPos).magnitudeSquared();
+
+                if (distanceSquared < minDistanceSquared) {
+                    closestIntersection = int.copy();
+                    minDistanceSquared = distanceSquared;
+                }
+            }
+
+            const vertices = capsuleShape.worldVertices;
+            for (let i = 0; i < vertices.length; i++) {
+                const v0 = vertices[i];
+                const v1 = vertices[(i + 1) % vertices.length];
+
+                const intersection = edgeIntersection(currentPos, nextPos, v0, v1);
+
+                if (intersection) {
+                    const distanceSquared = intersection.subNew(currentPos).magnitudeSquared();
+
+                    if (distanceSquared < minDistanceSquared) {
+                        closestIntersection = intersection.copy();
+                        minDistanceSquared = distanceSquared;
+                    }
+                }
+            }
+        }
+    }
+
+    if (closestIntersection) {
+        // TODO: if we move bullet to closestIntersection and shoot down the bullet sticks to the floor,
+        // probably something wron with polygon/circle collision, also if we move to bulletNewPos
+        // the bounce angle is wrong
+        const toBullet = currentPos.subNew(closestIntersection).unitVector();
+        const bulletNewPos = closestIntersection.addNew(toBullet.scaleNew(bulletShape.radius));
+        bullet.position = bulletNewPos.copy();
+        bullet.shape.updateAABB(bullet);
+        bullet.hasCCD = true;
+    }
+}
--- src/core/World.ts
+++ src/core/World.ts
@@ -7,16 +7,12 @@
  * Original project:
  * https://github.com/Sopiro
  */
+import * as CCD from '../collision/CCD';
 import * as Collision from '../collision/Collision';
 import { ContactManifold } from '../collision/ContactManifold';
 import { Joint } from '../joint/Joint';
 import Vec2 from '../math/Vec2';
 import Force from '../physics/Force';
-import { CapsuleShape } from '../shapes/CapsuleShape';
-import { CircleShape } from '../shapes/CircleShape';
-import { edgeCircleIntersection, edgeIntersection } from '../shapes/Edge';
-import { PolygonShape } from '../shapes/PolygonShape';
-import { ShapeType } from '../shapes/Shape';
 import * as Utils from '../utils/Utils';
 import { BODY_REMOVAL_THRESHOLD, MIN_BULLET_SPEED, SETTINGS } from './Constants';
 import RigidBody from './RigidBody';
@@ -177,123 +173,7 @@ export default class World {
     ccd(dt: number) {
         for (const body of this.bodies) {
             if (body.isBullet && body.velocity.magnitudeSquared() > MIN_BULLET_SPEED) {
-                const bulletShape = body.shape as CircleShape;
-                const currentPos = body.position.copy();
-                const nextPos = currentPos.addNew(body.velocity.scaleNew(dt));
-
-                let minDistanceSquared = Infinity;
-                let closestIntersection: Vec2 | undefined;
-
-                // TODO: We could cast two rays instead of one or check intersection by shifting up and down by radius
-                for (const other of this.bodies) {
-                    if (body.id === other.id || other.isBullet) continue;
-
-                    if (other.shapeType === ShapeType.BOX || other.shapeType === ShapeType.POLYGON) {
-                        const polygonShape = other.shape as PolygonShape;
-                        const vertices = polygonShape.worldVertices;
-
-                        for (let i = 0; i < vertices.length; i++) {
-                            const v0 = vertices[i];
-                            const v1 = vertices[(i + 1) % vertices.length];
-
-                            const intersection = edgeIntersection(currentPos, nextPos, v0, v1);
-
-                            if (intersection) {
-                                const distanceSquared = intersection.subNew(currentPos).magnitudeSquared();
-
-                                if (distanceSquared < minDistanceSquared) {
-                                    closestIntersection = intersection.copy();
-                                    minDistanceSquared = distanceSquared;
-                                }
-                            }
-                        }
-                    }
-
-                    if (other.shapeType === ShapeType.CIRCLE) {
-                        const circleShape = other.shape as CircleShape;
-                        const intersections = edgeCircleIntersection(
-                            currentPos,
-                            nextPos,
-                            other.position,
-                            circleShape.radius,
-                        );
-
-                        for (const int of intersections) {
-                            const distanceSquared = int.subNew(currentPos).magnitudeSquared();
-
-                            if (distanceSquared < minDistanceSquared) {
-                                closestIntersection = int.copy();
-                                minDistanceSquared = distanceSquared;
-                            }
-                        }
-                    }
-
-                    if (other.shapeType === ShapeType.CAPSULE) {
-                        const capsuleShape = other.shape as CapsuleShape;
-                        const topCirclePosition = capsuleShape.getTopCirclePosition(other);
-                        const bottomCirclePosition = capsuleShape.getBottomCirclePosition(other);
-
-                        const topCircleIntersections = edgeCircleIntersection(
-                            currentPos,
-                            nextPos,
-                            topCirclePosition,
-                            capsuleShape.radius,
-                        );
-
-                        for (const int of topCircleIntersections) {
-                            const distanceSquared = int.subNew(currentPos).magnitudeSquared();
-
-                            if (distanceSquared < minDistanceSquared) {
-                                closestIntersection = int.copy();
-                                minDistanceSquared = distanceSquared;
-                            }
-                        }
-
-                        const bottomCircleIntersections = edgeCircleIntersection(
-                            currentPos,
-                            nextPos,
-                            bottomCirclePosition,
-                            capsuleShape.radius,
-                        );
-
-                        for (const int of bottomCircleIntersections) {
-                            const distanceSquared = int.subNew(currentPos).magnitudeSquared();
-
-                            if (distanceSquared < minDistanceSquared) {
-                                closestIntersection = int.copy();
-                                minDistanceSquared = distanceSquared;
-                            }
-                        }
-
-                        const vertices = capsuleShape.worldVertices;
-                        for (let i = 0; i < vertices.length; i++) {
-                            const v0 = vertices[i];
-                            const v1 = vertices[(i + 1) % vertices.length];
-
-                            const intersection = edgeIntersection(currentPos, nextPos, v0, v1);
-
-                            if (intersection) {
-                                const distanceSquared = intersection.subNew(currentPos).magnitudeSquared();
-
-                                if (distanceSquared < minDistanceSquared) {
-                                    closestIntersection = intersection.copy();
-                                    minDistanceSquared = distanceSquared;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (closestIntersection) {
-                    // TODO: if we move bullet to closestIntersection and shoot down the bullet sticks to the floor,
-                    // probably something wron with polygon/circle collision, also if we move to bulletNewPos
-                    // the bounce angle is wrong
-                    const toBullet = currentPos.subNew(closestIntersection).unitVector();
-                    const bulletNewPos = closestIntersection.addNew(toBullet.scaleNew(bulletShape.radius));
-                    body.position = bulletNewPos.copy();
-                    body.shape.updateAABB(body);
-                    body.hasCCD = true;
-                }
+                CCD.resolveCCD(body, this.bodies, dt);
             }
         }
     }
