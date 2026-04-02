6f4d14adffa4c620d771a2fc6ed491a18175a78a
Samuel Dalvai <samuel.dalvai@gmail.com>
2026-04-02 20:00:00 +0200

    feat: add performance test on circle collision

--- __tests__/Performance.test.ts
+++ __tests__/Performance.test.ts
@@ -1,15 +1,19 @@
-import { RigidBody } from '../src/core/RigidBody';
-import { BoxShape } from '../src/shapes/BoxShape';
+// import * as Collision from '../src/collision/Collision';
+// import { RigidBody } from '../src/core/RigidBody';
+// import { CircleShape } from '../src/shapes/CircleShape';
 import * as Utils from '../src/utils/Utils';
+import * as Collision from '../src_new/collision/Collision';
+import { RigidBody } from '../src_new/core/RigidBody';
+import { CircleShape } from '../src_new/shapes/CircleShape';
 
 describe('Performance', () => {
     test('', () => {
         console.time('time');
-        const a = new RigidBody(new BoxShape(20, 20), 100, 100, 5);
-        const b = new RigidBody(new BoxShape(20, 20), 100, 100, 5);
+        const a = new RigidBody(new CircleShape(30), 100, 120, 5);
+        const b = new RigidBody(new CircleShape(30), 100, 100, 5);
 
-        for (let i = 0; i < 100000000; i++) {
-            Utils.pairKey(a, b);
+        for (let i = 0; i < 100000; i++) {
+            Collision.detectCollision(a, b);
         }
 
         console.timeEnd('time');
