775ce9c87a6a23048a8ce1f76ed41d8e35a2d8f7
Samuel Dalvai <samuel.dalvai@gmail.com>
2026-04-02 20:00:00 +0200

    feat: add performance test on capsule vs capsule

--- __tests__/Performance.test.ts
+++ __tests__/Performance.test.ts
@@ -2,39 +2,40 @@ import * as Collision from '../src/collision/Collision';
 import { RigidBody } from '../src/core/RigidBody';
 import { CircleShape } from '../src/shapes/CircleShape';
 import { BoxShape } from '../src/shapes/BoxShape';
+import { CapsuleShape } from '../src';
 
-describe('Performance circle vs circle', () => {
-    test('collision circle vs circle', () => {
-        console.time('collision');
-        const a = new RigidBody(new CircleShape(30), 100, 120, 5);
-        const b = new RigidBody(new CircleShape(30), 100, 100, 5);
+// describe('Performance circle vs circle', () => {
+//     test('collision circle vs circle', () => {
+//         console.time('collision');
+//         const a = new RigidBody(new CircleShape(30), 100, 120, 5);
+//         const b = new RigidBody(new CircleShape(30), 100, 100, 5);
 
-        for (let i = 0; i < 100000; i++) {
-            Collision.detectCollision(a, b);
-        }
+//         for (let i = 0; i < 100000; i++) {
+//             Collision.detectCollision(a, b);
+//         }
 
-        console.timeEnd('collision');
-    });
+//         console.timeEnd('collision');
+//     });
 
-    test('resolution circle vs circle', () => {
-        console.time('resolution');
-        const dt = 1 / 60;
-        const invDt = 1 / dt;
-        const a = new RigidBody(new CircleShape(30), 100, 120, 5);
-        const b = new RigidBody(new CircleShape(30), 100, 100, 5);
-        const manifold = Collision.detectCollision(a, b)!;
+//     test('resolution circle vs circle', () => {
+//         console.time('resolution');
+//         const dt = 1 / 60;
+//         const invDt = 1 / dt;
+//         const a = new RigidBody(new CircleShape(30), 100, 120, 5);
+//         const b = new RigidBody(new CircleShape(30), 100, 100, 5);
+//         const manifold = Collision.detectCollision(a, b)!;
 
-        for (let i = 0; i < 100000; i++) {
-            manifold.preSolve(invDt);
+//         for (let i = 0; i < 100000; i++) {
+//             manifold.preSolve(invDt);
 
-            for (let j = 0; j < 10; j++) {
-                manifold.solve();
-            }
-        }
+//             for (let j = 0; j < 10; j++) {
+//                 manifold.solve();
+//             }
+//         }
 
-        console.timeEnd('resolution');
-    });
-});
+//         console.timeEnd('resolution');
+//     });
+// });
 
 // describe('Performance box vs box', () => {
 //     test('collision box vs box', () => {
@@ -68,3 +69,36 @@ describe('Performance circle vs circle', () => {
 //         console.timeEnd('resolution');
 //     });
 // });
+
+describe('Performance capsule vs capsule', () => {
+    test('collision capsule vs capsule', () => {
+        console.time('collision');
+        const a = new RigidBody(new CapsuleShape(30, 30), 100, 120, 5);
+        const b = new RigidBody(new CapsuleShape(30, 30), 100, 100, 5);
+
+        for (let i = 0; i < 100000; i++) {
+            Collision.detectCollision(a, b);
+        }
+
+        console.timeEnd('collision');
+    });
+
+    test('resolution capsule vs capsule', () => {
+        console.time('resolution');
+        const dt = 1 / 60;
+        const invDt = 1 / dt;
+        const a = new RigidBody(new CapsuleShape(30, 30), 100, 120, 5);
+        const b = new RigidBody(new CapsuleShape(30, 30), 100, 100, 5);
+        const manifold = Collision.detectCollision(a, b)!;
+
+        for (let i = 0; i < 100000; i++) {
+            manifold.preSolve(invDt);
+
+            for (let j = 0; j < 10; j++) {
+                manifold.solve();
+            }
+        }
+
+        console.timeEnd('resolution');
+    });
+});
