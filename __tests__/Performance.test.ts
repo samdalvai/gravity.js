e66c70198e8046cd3de2ca55c37b67a41251ea7d
Samuel Dalvai <samuel.dalvai@gmail.com>
2026-04-02 20:00:00 +0200

    refactor: temporary disable box vs box performance test

--- __tests__/Performance.test.ts
+++ __tests__/Performance.test.ts
@@ -36,35 +36,35 @@ describe('Performance circle vs circle', () => {
     });
 });
 
-describe('Performance box vs box', () => {
-    test('collision box vs box', () => {
-        console.time('collision');
-        const a = new RigidBody(new BoxShape(60, 60), 100, 120, 5);
-        const b = new RigidBody(new BoxShape(60, 60), 100, 100, 5);
+// describe('Performance box vs box', () => {
+//     test('collision box vs box', () => {
+//         console.time('collision');
+//         const a = new RigidBody(new BoxShape(60, 60), 100, 120, 5);
+//         const b = new RigidBody(new BoxShape(60, 60), 100, 100, 5);
 
-        for (let i = 0; i < 100000; i++) {
-            Collision.detectCollision(a, b);
-        }
+//         for (let i = 0; i < 100000; i++) {
+//             Collision.detectCollision(a, b);
+//         }
 
-        console.timeEnd('collision');
-    });
+//         console.timeEnd('collision');
+//     });
 
-    test('resolution box vs box', () => {
-        console.time('resolution');
-        const dt = 1 / 60;
-        const invDt = 1 / dt;
-        const a = new RigidBody(new BoxShape(60, 60), 100, 120, 5);
-        const b = new RigidBody(new BoxShape(60, 60), 100, 100, 5);
-        const manifold = Collision.detectCollision(a, b)!;
+//     test('resolution box vs box', () => {
+//         console.time('resolution');
+//         const dt = 1 / 60;
+//         const invDt = 1 / dt;
+//         const a = new RigidBody(new BoxShape(60, 60), 100, 120, 5);
+//         const b = new RigidBody(new BoxShape(60, 60), 100, 100, 5);
+//         const manifold = Collision.detectCollision(a, b)!;
 
-        for (let i = 0; i < 100000; i++) {
-            manifold.preSolve(invDt);
+//         for (let i = 0; i < 100000; i++) {
+//             manifold.preSolve(invDt);
 
-            for (let j = 0; j < 10; j++) {
-                manifold.solve();
-            }
-        }
+//             for (let j = 0; j < 10; j++) {
+//                 manifold.solve();
+//             }
+//         }
 
-        console.timeEnd('resolution');
-    });
-});
+//         console.timeEnd('resolution');
+//     });
+// });
