ccfd260d80056c97c1dfae83490f298c26c55031
Samuel Dalvai <samuel.dalvai@gmail.com>
2026-04-03 20:00:00 +0200

    feat: add backup for collision code

--- src/core/World.ts
+++ src/core/World.ts
@@ -9,7 +9,7 @@
  */
 import * as CCD from '../collision/CCD';
 import * as Collision from '../collision/Collision';
-import { ContactManifold } from '../collision/contact';
+import { ContactManifold } from '../collision/Contact';
 import Force from '../force/Force';
 import { Joint } from '../joint/Joint';
 import { Vec2 } from '../math/Vec2';
