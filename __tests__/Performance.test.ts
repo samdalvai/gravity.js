17f23ad0efd46312ed5323044dbb839dbd7cd67b
Samuel Dalvai <samuel.dalvai@gmail.com>
2026-04-09 20:00:00 +0200

    feat: add basic performance harness for performance tests

--- __tests__/Performance.test.ts
+++ __tests__/Performance.test.ts
@@ -1,18 +1,50 @@
-import { describe, test } from '@jest/globals';
+import { describe, expect, test } from '@jest/globals';
+
+import { CircleShape, RigidBody } from '../src';
+import * as Collision from '../src/collision/NarrowPhase';
+
+const DT = 1 / 60;
+const INV_DT = 1 / DT;
+const SOLVER_ITERATIONS = 20;
+const TEST_ITERATIONS = 100_000;
 
 describe('Performance test', () => {
     test('Implementation 1', () => {
-        console.time('perf');
+        console.time('normal');
 
-        // ...
+        const a = new RigidBody(new CircleShape(30), 0, 0, 1.0);
+        const b = new RigidBody(new CircleShape(30), 0, 0, 1.0);
+        const maifold = Collision.detectCollision(a, b)!;
+        expect(maifold).not.toBeNull();
 
-        console.timeEnd('perf');
+        maifold.preSolve(INV_DT);
+
+        // Solve constraints
+        for (let x = 0; x < TEST_ITERATIONS; x++) {
+            for (let i = 0; i < SOLVER_ITERATIONS; i++) {
+                maifold.solve();
+            }
+        }
+
+        console.timeEnd('normal');
     });
 
     test('Implementation 2', () => {
         console.time('perf');
 
-        // ...
+        const a = new RigidBody(new CircleShape(30), 0, 0, 1.0);
+        const b = new RigidBody(new CircleShape(30), 0, 0, 1.0);
+        const maifold = Collision.detectCollision(a, b)!;
+        expect(maifold).not.toBeNull();
+
+        maifold.preSolve(INV_DT);
+
+        // Solve constraints
+        for (let x = 0; x < TEST_ITERATIONS; x++) {
+            for (let i = 0; i < SOLVER_ITERATIONS; i++) {
+                maifold.solve();
+            }
+        }
 
         console.timeEnd('perf');
     });
