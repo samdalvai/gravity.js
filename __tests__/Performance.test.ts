fb9ebafc8d80cd242c2b87bc854aec2b7f07b600
Samuel Dalvai <samuel.dalvai@gmail.com>
2026-04-09 20:00:00 +0200

    feat: add perf version of distance joint

--- __tests__/Performance.test.ts
+++ __tests__/Performance.test.ts
@@ -2,6 +2,8 @@ import { describe, expect, test } from '@jest/globals';
 
 import { BoxShape, CircleShape, RigidBody } from '../src';
 import * as Collision from '../src/collision/NarrowPhase';
+import { DistanceJoint } from '../src/joint/DistanceJoint';
+import { DistanceJoint as DistanceJointPerf } from '../src/joint/DistanceJoint.perf';
 
 const DT = 1 / 60;
 const INV_DT = 1 / DT;
@@ -12,21 +14,20 @@ describe('Performance test', () => {
     test('Implementation 1', () => {
         console.time('normal');
 
-        // const a = new RigidBody(new CircleShape(30), 0, 0, 1.0);
-        // const b = new RigidBody(new CircleShape(30), 0, 0, 1.0);
-        const a = new RigidBody(new BoxShape(60, 60), 0, 0, 1.0);
-        const b = new RigidBody(new BoxShape(60, 60), 0, 0, 1.0);
-        // const manifold = Collision.detectCollision(a, b)!;
-        // expect(manifold).not.toBeNull();
+        const a = new RigidBody(new CircleShape(30), 0, 0, 1.0);
+        const b = new RigidBody(new CircleShape(30), 0, 0, 1.0);
+        // const a = new RigidBody(new BoxShape(60, 60), 0, 0, 1.0);
+        // const b = new RigidBody(new BoxShape(60, 60), 0, 0, 1.0);
+        const constraint = new DistanceJoint(a, b, a.position, b.position);
+        expect(constraint).not.toBeNull();
 
-        // manifold.preSolve(INV_DT);
-
-        // // Solve constraints
-        // for (let x = 0; x < TEST_ITERATIONS; x++) {
-        //     for (let i = 0; i < SOLVER_ITERATIONS; i++) {
-        //         manifold.solve();
-        //     }
-        // }
+        // Solve constraints
+        for (let x = 0; x < TEST_ITERATIONS; x++) {
+            constraint.preSolve(INV_DT);
+            for (let i = 0; i < SOLVER_ITERATIONS; i++) {
+                constraint.solve();
+            }
+        }
 
         console.timeEnd('normal');
     });
@@ -34,21 +35,20 @@ describe('Performance test', () => {
     test('Implementation 2', () => {
         console.time('perf');
 
-        // const a = new RigidBody(new CircleShape(30), 0, 0, 1.0);
-        // const b = new RigidBody(new CircleShape(30), 0, 0, 1.0);
-        const a = new RigidBody(new BoxShape(60, 60), 0, 0, 1.0);
-        const b = new RigidBody(new BoxShape(60, 60), 0, 0, 1.0);
-        // const manifold = CollisionPerf.detectCollision(a, b)!;
-        // expect(manifold).not.toBeNull();
+        const a = new RigidBody(new CircleShape(30), 0, 0, 1.0);
+        const b = new RigidBody(new CircleShape(30), 0, 0, 1.0);
+        // const a = new RigidBody(new BoxShape(60, 60), 0, 0, 1.0);
+        // const b = new RigidBody(new BoxShape(60, 60), 0, 0, 1.0);
+        const constraint = new DistanceJointPerf(a, b, a.position, b.position);
+        expect(constraint).not.toBeNull();
 
-        // manifold.preSolve(INV_DT);
-
-        // // Solve constraints
-        // for (let x = 0; x < TEST_ITERATIONS; x++) {
-        //     for (let i = 0; i < SOLVER_ITERATIONS; i++) {
-        //         manifold.solve();
-        //     }
-        // }
+        // Solve constraints
+        for (let x = 0; x < TEST_ITERATIONS; x++) {
+            constraint.preSolve(INV_DT);
+            for (let i = 0; i < SOLVER_ITERATIONS; i++) {
+                constraint.solve();
+            }
+        }
 
         console.timeEnd('perf');
     });
