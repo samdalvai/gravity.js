bcc2720499cd6bf21c008e1ff0fd7872d6e84bb1
Samuel Dalvai <samuel.dalvai@gmail.com>
2026-04-13 20:00:00 +0200

    feat: add all bodies pairs to benchmark

--- benchmark/functions.ts
+++ benchmark/functions.ts
@@ -1,19 +1,39 @@
-import { CircleShape, RigidBody } from '../src';
-import { collideCircles, collideCirclesScalar } from '../src/collision/NarrowPhase';
+import { BoxShape, CapsuleShape, CircleShape, RigidBody, SETTINGS, SegmentShape } from '../src';
 
-export const ITERATIONS = 10_000;
+export const ITERATIONS = 20_000;
 
-const a = new RigidBody(new CircleShape(30), 0, 0, 1.0);
-const b = new RigidBody(new CircleShape(30), 30, 0, 1.0);
+// const a = new RigidBody(new CircleShape(30), 0, 0, 1.0);
+// const b = new RigidBody(new CircleShape(30), 30, 0, 1.0);
+
+// const a = new RigidBody(new BoxShape(60, 60), 0, 0, 1.0);
+// const b = new RigidBody(new BoxShape(60, 60), 30, 0, 1.0);
+
+// const a = new RigidBody(new BoxShape(60, 60), 0, 0, 1.0);
+// const b = new RigidBody(new CircleShape(30), 30, 0, 1.0);
+
+// const a = new RigidBody(new BoxShape(60, 60), 0, 0, 1.0);
+// const b = new RigidBody(new CapsuleShape(40, 10), 0, 20, 1.0);
+
+// const a = new RigidBody(new CircleShape(30), 0, 0, 1.0);
+// const b = new RigidBody(new CapsuleShape(40, 10), 0, 20, 1.0);
+
+// const a = new RigidBody(new BoxShape(60, 60), 0, 0, 1.0);
+// const b = new RigidBody(new SegmentShape(40, true), 0, 20, 1.0);
+
+// const a = new RigidBody(new CircleShape(30), 0, 0, 1.0);
+// const b = new RigidBody(new SegmentShape(40, true), 0, 20, 1.0);
+
+// const a = new RigidBody(new CapsuleShape(40, 10), 0, 0, 1.0);
+// const b = new RigidBody(new SegmentShape(40, true), 0, 20, 1.0);
 
 export function runOriginal() {
     for (let i = 0; i < ITERATIONS; i++) {
-        collideCircles(a, b);
+        //
     }
 }
 
 export function runModified() {
     for (let i = 0; i < ITERATIONS; i++) {
-        collideCirclesScalar(a, b);
+        //
     }
 }
