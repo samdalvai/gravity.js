17b5e534f7417d4fbe866ccc34a0f78d97f215fa
Samuel Dalvai <samuel.dalvai@gmail.com>
2026-04-13 20:00:00 +0200

    feat: benchmar collision vs scalar

--- benchmark/functions.ts
+++ benchmark/functions.ts
@@ -1,9 +1,11 @@
-import { BoxShape, CapsuleShape, CircleShape, RigidBody, SETTINGS, SegmentShape } from '../src';
+import { BoxShape, CapsuleShape, CircleShape, RigidBody, SegmentShape } from '../src';
+import { detectCollision } from '../src/collision/NarrowPhase';
+import { detectCollision as detectCollisionScalar } from '../src/collision/NarrowPhase.scalar';
 
 export const ITERATIONS = 20_000;
 
-// const a = new RigidBody(new CircleShape(30), 0, 0, 1.0);
-// const b = new RigidBody(new CircleShape(30), 30, 0, 1.0);
+const a = new RigidBody(new CircleShape(30), 0, 0, 1.0);
+const b = new RigidBody(new CircleShape(30), 30, 0, 1.0);
 
 // const a = new RigidBody(new BoxShape(60, 60), 0, 0, 1.0);
 // const b = new RigidBody(new BoxShape(60, 60), 30, 0, 1.0);
@@ -28,12 +30,12 @@ export const ITERATIONS = 20_000;
 
 export function runOriginal() {
     for (let i = 0; i < ITERATIONS; i++) {
-        //
+        detectCollision(a, b);
     }
 }
 
 export function runModified() {
     for (let i = 0; i < ITERATIONS; i++) {
-        //
+        detectCollisionScalar(a, b);
     }
 }
