aac75d39727214740c34110591577721253fdf86
Samuel Dalvai <samuel.dalvai@gmail.com>
2026-04-14 20:00:00 +0200

    feat: add benchmark on packed arrays

--- benchmark/functions.ts
+++ benchmark/functions.ts
@@ -1,7 +1,47 @@
+import { Vec2 } from '../src';
+import { randomNumber } from '../src/utils/Utils';
+
+const NUM_BODIES = 10_000;
+
+class TestBody {
+    velocity: Vec2;
+
+    constructor(x: number, y: number) {
+        this.velocity = new Vec2(x, y);
+    }
+}
+
+const bodies: TestBody[] = [];
+
+// const velocitiesX = new Float64Array(NUM_BODIES);
+// const velocitiesY = new Float64Array(NUM_BODIES);
+const velocitiesXY = new Float64Array(NUM_BODIES * 2);
+
+for (let i = 0; i < NUM_BODIES; i++) {
+    const x = randomNumber(-100, 100);
+    const y = randomNumber(-100, 100);
+
+    bodies.push(new TestBody(x, y));
+    // velocitiesX[i] = x;
+    // velocitiesY[i] = y;
+    velocitiesXY[i] = x;
+    velocitiesXY[i * 2] = y;
+}
+
 export function runOriginal() {
-    //
+    for (let i = 0; i < NUM_BODIES; i++) {
+        const velocity = bodies[i].velocity;
+        velocity.x += 1;
+        velocity.y += 1;
+    }
 }
 
 export function runModified() {
-    //
+    for (let i = 0; i < NUM_BODIES; i++) {
+        // velocitiesX[i] += 1;
+        // velocitiesY[i] += 1;
+        // Version above is faster
+        velocitiesXY[i] += 1;
+        velocitiesXY[i * 2] += 1;
+    }
 }
