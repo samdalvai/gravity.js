7c5bc7d86a5c6cceaf14c2fd909203917d85c498
Samuel Dalvai <samuel.dalvai@gmail.com>
2026-04-16 20:00:00 +0200

    feat: update weld joint code

--- src/joint/WeldJoint.ts
+++ src/joint/WeldJoint.ts
@@ -1,26 +1,27 @@
-import { Joint } from './joint.js';
-import { Matrix3, Vector2, Vector3 } from './math.js';
-import { RigidBody } from './rigidbody.js';
-import { Settings } from './settings.js';
-import * as Util from './util.js';
+import { SETTINGS } from '../core/Constants';
+import { RigidBody } from '../core/RigidBody';
+import { Mat3 } from '../math/Mat3';
+import { Vec2 } from '../math/Vec2';
+import { Vec3 } from '../math/Vec3';
+import { Joint } from './Joint';
 
 // Revolute joint + Angle joint
 export class WeldJoint extends Joint {
-    public localAnchorA: Vector2;
-    public localAnchorB: Vector2;
+    public localAnchorA: Vec2;
+    public localAnchorB: Vec2;
 
     public initialAngleOffset: number;
 
-    private ra!: Vector2;
-    private rb!: Vector2;
-    private m!: Matrix3;
-    private bias!: Vector3;
-    private impulseSum: Vector3 = new Vector3();
+    private ra!: Vec2;
+    private rb!: Vec2;
+    private m!: Mat3;
+    private bias!: Vec3;
+    private impulseSum: Vec3 = new Vec3();
 
     constructor(
         bodyA: RigidBody,
         bodyB: RigidBody,
-        anchor: Vector2 = Util.mid(bodyA.position, bodyB.position),
+        anchor: Vec2 = bodyA.position.lerp(bodyB.position, 0.5),
         frequency = -1,
         dampingRatio = 1.0,
         jointMass = -1,
@@ -28,8 +29,8 @@ export class WeldJoint extends Joint {
         super(bodyA, bodyB, frequency, dampingRatio, jointMass);
 
         this.initialAngleOffset = bodyB.rotation - bodyA.rotation;
-        this.localAnchorA = this.bodyA.globalToLocal.mulVector2(anchor, 1);
-        this.localAnchorB = this.bodyB.globalToLocal.mulVector2(anchor, 1);
+        this.localAnchorA = this.bodyA.worldPointToLocal(anchor);
+        this.localAnchorB = this.bodyB.worldPointToLocal(anchor);
 
         this.drawAnchor = false;
         this.drawConnectionLine = false;
@@ -41,34 +42,34 @@ export class WeldJoint extends Joint {
         //     [ 0,        -1, 0,        1] // Angle
         // M = (J · M^-1 · J^t)^-1
 
-        this.ra = this.bodyA.localToGlobal.mulVector2(this.localAnchorA, 0);
-        this.rb = this.bodyB.localToGlobal.mulVector2(this.localAnchorB, 0);
+        this.ra = this.bodyA.localPointToWorld(this.localAnchorA);
+        this.rb = this.bodyB.localPointToWorld(this.localAnchorB);
 
-        const k = new Matrix3();
+        const k = new Mat3();
 
         k.m00 =
-            this.bodyA.inverseMass +
-            this.bodyB.inverseMass +
-            this.bodyA.inverseInertia * this.ra.y * this.ra.y +
-            this.bodyB.inverseInertia * this.rb.y * this.rb.y;
+            this.bodyA.invMass +
+            this.bodyB.invMass +
+            this.bodyA.invI * this.ra.y * this.ra.y +
+            this.bodyB.invI * this.rb.y * this.rb.y;
 
-        k.m01 = -this.bodyA.inverseInertia * this.ra.y * this.ra.x - this.bodyB.inverseInertia * this.rb.y * this.rb.x;
+        k.m01 = -this.bodyA.invI * this.ra.y * this.ra.x - this.bodyB.invI * this.rb.y * this.rb.x;
 
-        k.m10 = -this.bodyA.inverseInertia * this.ra.x * this.ra.y - this.bodyB.inverseInertia * this.rb.x * this.rb.y;
+        k.m10 = -this.bodyA.invI * this.ra.x * this.ra.y - this.bodyB.invI * this.rb.x * this.rb.y;
 
         k.m11 =
-            this.bodyA.inverseMass +
-            this.bodyB.inverseMass +
-            this.bodyA.inverseInertia * this.ra.x * this.ra.x +
-            this.bodyB.inverseInertia * this.rb.x * this.rb.x;
+            this.bodyA.invMass +
+            this.bodyB.invMass +
+            this.bodyA.invI * this.ra.x * this.ra.x +
+            this.bodyB.invI * this.rb.x * this.rb.x;
 
-        k.m02 = -this.bodyA.inverseInertia * this.ra.y - this.bodyB.inverseInertia * this.rb.y;
-        k.m12 = this.bodyA.inverseInertia * this.ra.x + this.bodyB.inverseInertia * this.rb.x;
+        k.m02 = -this.bodyA.invI * this.ra.y - this.bodyB.invI * this.rb.y;
+        k.m12 = this.bodyA.invI * this.ra.x + this.bodyB.invI * this.rb.x;
 
-        k.m20 = -this.bodyA.inverseInertia * this.ra.y - this.bodyB.inverseInertia * this.rb.y;
-        k.m21 = this.bodyA.inverseInertia * this.ra.x + this.bodyB.inverseInertia * this.rb.x;
+        k.m20 = -this.bodyA.invI * this.ra.y - this.bodyB.invI * this.rb.y;
+        k.m21 = this.bodyA.invI * this.ra.x + this.bodyB.invI * this.rb.x;
 
-        k.m22 = this.bodyA.inverseInertia + this.bodyB.inverseInertia;
+        k.m22 = this.bodyA.invI + this.bodyB.invI;
 
         k.m00 += this.gamma;
         k.m11 += this.gamma;
@@ -76,17 +77,16 @@ export class WeldJoint extends Joint {
 
         this.m = k.inverted();
 
-        const pa = this.bodyA.position.add(this.ra);
-        const pb = this.bodyB.position.add(this.rb);
+        const pa = this.bodyA.position.addNew(this.ra);
+        const pb = this.bodyB.position.addNew(this.rb);
 
-        const error01 = pb.sub(pa);
+        const error01 = pb.subNew(pa);
         const error2 = this.bodyB.rotation - this.bodyA.rotation - this.initialAngleOffset;
 
-        if (Settings.positionCorrection)
-            this.bias = new Vector3(error01.x, error01.y, error2).mul(this.beta * Settings.inv_dt);
-        else this.bias = new Vector3(0.0, 0.0, 0.0);
+        if (SETTINGS.positionCorrection) this.bias = new Vec3(error01.x, error01.y, error2).mul(this.beta * invDt);
+        else this.bias = new Vec3(0.0, 0.0, 0.0);
 
-        if (Settings.warmStarting) this.applyImpulse(this.impulseSum);
+        if (SETTINGS.warmStarting) this.applyImpulse(this.impulseSum);
     }
 
     override solve() {
@@ -94,35 +94,35 @@ export class WeldJoint extends Joint {
         // Pc = J^t * λ (λ: lagrangian multiplier)
         // λ = (J · M^-1 · J^t)^-1 ⋅ -(J·v+b)
 
-        const jv01 = this.bodyB.linearVelocity
+        const jv01 = this.bodyB.velocity
             .add(Util.cross(this.bodyB.angularVelocity, this.rb))
-            .sub(this.bodyA.linearVelocity.add(Util.cross(this.bodyA.angularVelocity, this.ra)));
+            .sub(this.bodyA.velocity.add(Util.cross(this.bodyA.angularVelocity, this.ra)));
         const jv2 = this.bodyB.angularVelocity - this.bodyA.angularVelocity;
 
-        const jv = new Vector3(jv01.x, jv01.y, jv2);
+        const jv = new Vec3(jv01.x, jv01.y, jv2);
 
-        const lambda = this.m.mulVector3(jv.add(this.bias).add(this.impulseSum.mul(this.gamma)).inverted());
+        const lambda = this.m.mulVec3(jv.add(this.bias).add(this.impulseSum.mul(this.gamma)).inverted());
 
         this.applyImpulse(lambda);
 
         if (Settings.warmStarting) this.impulseSum = this.impulseSum.add(lambda);
     }
 
-    protected override applyImpulse(lambda: Vector3) {
+    private applyImpulse(lambda: Vec3) {
         // V2 = V2' + M^-1 ⋅ Pc
         // Pc = J^t ⋅ λ
 
-        const lambda01 = new Vector2(lambda.x, lambda.y);
+        const lambda01 = new Vec2(lambda.x, lambda.y);
         const lambda2 = lambda.z;
 
         // Solve for point-to-point constraint
-        this.bodyA.linearVelocity = this.bodyA.linearVelocity.sub(lambda01.mul(this.bodyA.inverseMass));
-        this.bodyA.angularVelocity = this.bodyA.angularVelocity - this.bodyA.inverseInertia * this.ra.cross(lambda01);
-        this.bodyB.linearVelocity = this.bodyB.linearVelocity.add(lambda01.mul(this.bodyB.inverseMass));
-        this.bodyB.angularVelocity = this.bodyB.angularVelocity + this.bodyB.inverseInertia * this.rb.cross(lambda01);
+        this.bodyA.velocity = this.bodyA.velocity.sub(lambda01.mul(this.bodyA.invMass));
+        this.bodyA.angularVelocity = this.bodyA.angularVelocity - this.bodyA.invI * this.ra.cross(lambda01);
+        this.bodyB.velocity = this.bodyB.velocity.add(lambda01.mul(this.bodyB.invMass));
+        this.bodyB.angularVelocity = this.bodyB.angularVelocity + this.bodyB.invI * this.rb.cross(lambda01);
 
         // Solve for angle constraint
-        this.bodyA.angularVelocity = this.bodyA.angularVelocity - lambda2 * this.bodyA.inverseInertia;
-        this.bodyB.angularVelocity = this.bodyB.angularVelocity + lambda2 * this.bodyB.inverseInertia;
+        this.bodyA.angularVelocity = this.bodyA.angularVelocity - lambda2 * this.bodyA.invI;
+        this.bodyB.angularVelocity = this.bodyB.angularVelocity + lambda2 * this.bodyB.invI;
     }
 }
--- src/math/Mat3.ts
+++ src/math/Mat3.ts
@@ -1,7 +1,7 @@
 import { Vec2 } from './Vec2';
 import { Vec3 } from './Vec3';
 
-export class Matrix3 {
+export class Mat3 {
     m00: number;
     m01: number;
     m02: number;
@@ -36,8 +36,8 @@ export class Matrix3 {
         this.m22 = 1;
     }
 
-    copy(): Matrix3 {
-        const res = new Matrix3();
+    copy(): Mat3 {
+        const res = new Mat3();
 
         res.m00 = this.m00;
         res.m01 = this.m01;
@@ -52,8 +52,8 @@ export class Matrix3 {
         return res;
     }
 
-    mulMatrix(right: Matrix3): Matrix3 {
-        const res = new Matrix3();
+    mulMatrix(right: Mat3): Mat3 {
+        const res = new Mat3();
 
         res.m00 = this.m00 * right.m00 + this.m01 * right.m10 + this.m02 * right.m20;
         res.m01 = this.m00 * right.m01 + this.m01 * right.m11 + this.m02 * right.m21;
@@ -89,19 +89,19 @@ export class Matrix3 {
         return res;
     }
 
-    scale(x: number, y: number): Matrix3 {
-        const scale = new Matrix3();
+    scale(x: number, y: number): Mat3 {
+        const scale = new Mat3();
         scale.m00 = x;
         scale.m11 = y;
 
         return this.mulMatrix(scale);
     }
 
-    rotate(r: number): Matrix3 {
+    rotate(r: number): Mat3 {
         const sin = Math.sin(r);
         const cos = Math.cos(r);
 
-        const res = new Matrix3();
+        const res = new Mat3();
 
         res.m00 = cos;
         res.m01 = -sin;
@@ -111,8 +111,8 @@ export class Matrix3 {
         return this.mulMatrix(res);
     }
 
-    translate(x: number, y: number): Matrix3 {
-        const res = new Matrix3();
+    translate(x: number, y: number): Mat3 {
+        const res = new Mat3();
 
         res.m02 = x;
         res.m12 = y;
@@ -120,8 +120,8 @@ export class Matrix3 {
         return this.mulMatrix(res);
     }
 
-    inverted(): Matrix3 {
-        const res = new Matrix3();
+    inverted(): Mat3 {
+        const res = new Mat3();
 
         const det =
             this.m00 * (this.m11 * this.m22 - this.m21 * this.m12) -
