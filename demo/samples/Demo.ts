a99f75f0820ae555159119098b1fbcc06ad4862f
Samuel Dalvai <samuel.dalvai@gmail.com>
2026-04-16 20:00:00 +0200

    feat: add blueprint for breakable joints demo

--- demo/samples/Demo.ts
+++ demo/samples/Demo.ts
@@ -56,6 +56,7 @@ export default class Demo {
         'Demo 14: 1000 Random convex shapes',
         'Demo 15: Black hole orbit',
         'Demo 16: Welded boxes',
+        'Demo 17: Breakable joints',
     ];
 
     static generateFloor(world: World, app: Application): RigidBody {
@@ -1051,6 +1052,66 @@ export default class Demo {
         }
     };
 
+    static demo17 = (world: World, app: Application) => {
+        // Demo 17: Breakable Joints
+        app.setBackground('background');
+        this.generateFloor(world, app);
+
+        const boxWidth = 20;
+        const boxRows = 10;
+        const xOffset = 250;
+        const yOffset = -250;
+        const spacing = 1;
+
+        const boxes: RigidBody[][] = [];
+
+        for (let i = 0; i < boxRows; i++) {
+            boxes[i] = [];
+
+            for (let j = 0; j < boxRows; j++) {
+                const box = Bodies.box({
+                    width: boxWidth,
+                    height: boxWidth,
+                    x: boxWidth * i + spacing * i + xOffset,
+                    y: boxWidth * j + spacing * j + yOffset,
+                    mass: 1,
+                    restitution: 0,
+                });
+
+                world.addBody(box);
+                boxes[i][j] = box;
+            }
+        }
+
+        // Create weld joints between adjacent boxes
+        const jointFrequency = 30;
+        const jointDamping = 0.5;
+
+        for (let i = 0; i < boxRows; i++) {
+            for (let j = 0; j < boxRows; j++) {
+                const current = boxes[i][j];
+
+                // weld to the box on the right
+                if (i + 1 < boxRows) {
+                    const right = boxes[i + 1][j];
+                    const weld = new WeldJoint(current, right, undefined, jointFrequency, jointDamping);
+                    weld.drawAnchor = true;
+                    weld.drawConnectionLine = true;
+                    world.addJoint(weld);
+                }
+
+                // weld to the box below
+                if (j + 1 < boxRows) {
+                    const below = boxes[i][j + 1];
+                    const weld = new WeldJoint(current, below, undefined, jointFrequency, jointDamping);
+                    weld.drawAnchor = true;
+                    weld.drawConnectionLine = true;
+                    world.addJoint(weld);
+                }
+            }
+        }
+    };
+
     static demoFunctions = [
         this.demo0,
         this.demo1,
@@ -1069,5 +1130,6 @@ export default class Demo {
         this.demo14,
         this.demo15,
         this.demo16,
+        this.demo17,
     ];
 }
