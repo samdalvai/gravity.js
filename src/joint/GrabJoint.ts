3e278f902cedb055b6a7c43ab8ba785e82e1c3dd
Samuel Dalvai <samuel.dalvai@gmail.com>
2026-04-17 20:00:00 +0200

    feat: add GrabJoint

---
+++ src/joint/GrabJoint.ts
@@ -0,0 +1,72 @@
+import { SETTINGS } from '../core/Constants';
+import { RigidBody } from '../core/RigidBody';
+import { Matrix2 } from '../math/Matrix2';
+import { Vec2 } from '../math/Vec2';
+import { Joint } from './Joint';
+
+export class GrabJoint extends Joint {
+    public localAnchor: Vec2;
+    public target: Vec2;
+
+    private r!: Vec2;
+    private m!: Matrix2;
+    private bias!: Vec2;
+    private impulseSum: Vec2 = new Vec2();
+
+    constructor(body: RigidBody, anchor: Vec2, target: Vec2, frequency = 0.8, dampingRatio = 0.6, jointMass = -1) {
+        super(body, body, frequency, dampingRatio, jointMass);
+
+        this.localAnchor = this.bodyA.worldPointToLocal(anchor);
+        this.target = target;
+    }
+
+    override preSolve(): void {
+        // Calculate Jacobian J and effective mass M
+        // J = [I, skew(r)]
+        // M = (J · M^-1 · J^t)^-1
+
+        this.r = this.bodyA.localPointToWorld(this.localAnchor);
+        const p = this.bodyA.position.addNew(this.r);
+
+        const k = new Matrix2();
+
+        k.m00 = this.bodyA.invMass + this.bodyA.invI * this.r.y * this.r.y;
+        k.m01 = -this.bodyA.invI * this.r.y * this.r.x;
+        k.m10 = -this.bodyA.invI * this.r.x * this.r.y;
+        k.m11 = this.bodyA.invMass + this.bodyA.invI * this.r.x * this.r.x;
+
+        k.m00 += this.gamma;
+        k.m11 += this.gamma;
+
+        this.m = k.inverted();
+
+        const error = p.subNew(this.target);
+
+        if (SETTINGS.positionCorrection) this.bias = error.scaleNew(this.beta * SETTINGS.invDt);
+        else this.bias = new Vec2(0.0, 0.0);
+
+        if (SETTINGS.warmStarting) this.applyImpulse(this.impulseSum);
+    }
+
+    override solve(): void {
+        // Calculate corrective impulse: Pc
+        // Pc = J^t · λ (λ: lagrangian multiplier)
+        // λ = (J · M^-1 · J^t)^-1 ⋅ -(J·v+b)
+
+        const jv: Vec2 = this.bodyA.velocity.addNew(this.r.crossScalar(this.bodyA.angularVelocity));
+
+        const lambda = this.m.mulVector(jv.addNew(this.bias).addNew(this.impulseSum.scaleNew(this.gamma)).negateNew());
+
+        this.applyImpulse(lambda);
+
+        if (SETTINGS.warmStarting) this.impulseSum = this.impulseSum.addNew(lambda);
+    }
+
+    private applyImpulse(lambda: Vec2): void {
+        // V2 = V2' + M^-1 ⋅ Pc
+        // Pc = J^t ⋅ λ
+
+        this.bodyA.velocity = this.bodyA.velocity.addNew(lambda.scaleNew(this.bodyA.invMass));
+        this.bodyA.angularVelocity = this.bodyA.angularVelocity + this.bodyA.invI * this.r.cross(lambda);
+    }
+}
