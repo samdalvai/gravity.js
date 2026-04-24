bdb131d9af00a0dc2de4dd3c1145b4c60f273b2b
Samuel Dalvai <samuel.dalvai@gmail.com>
2026-04-24 20:00:00 +0200

    feat: log num contacts in benchmark

--- benchmark/functions.ts
+++ benchmark/functions.ts
@@ -1,17 +1,17 @@
-import { BodiesFactory, Utils, Vec2, World } from '../src';
+import { BodiesFactory, Utils, Vec2, World as WorldNew } from '../src';
 import { BodiesFactory as BodiesFactoryOld, Vec2 as Vec2Old, World as WorldOld } from '../src_old';
 
 declare const process: {
     on(event: 'exit', listener: () => void): void;
 };
 
-const world = new World(-9.8);
+const worldNew = new WorldNew(-9.8);
 const worldOld = new WorldOld(-9.8);
 
 const numBodies = 1_000;
 
 for (let i = 0; i < numBodies; i++) {
-    const x = Utils.randomNumber(-1000, 1000);
+    const x = Utils.randomNumber(-250, 250);
     const y = Utils.randomNumber(-500, 500);
 
     const p = BodiesFactory.circle({
@@ -28,24 +28,31 @@ for (let i = 0; i < numBodies; i++) {
         mass: 1,
     });
 
-    world.addBody(p);
+    worldNew.addBody(p);
     worldOld.addBody(pOld);
 }
 
 const floor = BodiesFactory.box({ width: 5000, height: 50, x: 0, y: -600, mass: 0 });
-world.addBody(floor);
+worldNew.addBody(floor);
 
 const floorOld = BodiesFactoryOld.box({ width: 5000, height: 50, x: 0, y: -600, mass: 0 });
 worldOld.addBody(floorOld);
 
 const DT = 1 / 60;
 
+const warmupIterations = 1_000;
+
+for (let i = 0; i < warmupIterations; i++) {
+    worldOld.update(DT);
+    worldNew.update(DT);
+}
+
 export function runOriginal() {
     worldOld.update(DT);
 }
 
 export function runModified() {
-    world.update(DT);
+    worldNew.update(DT);
 }
 
 process.on('exit', () => {
@@ -53,4 +60,8 @@ process.on('exit', () => {
     console.log('Vec2 allocations');
     console.log(`original: ${Vec2.allocations.toLocaleString()}`);
     console.log(`modified: ${Vec2Old.allocations.toLocaleString()}`);
+
+    console.log('Contacts');
+    console.log(`original: ${worldOld.getManifolds().length}`);
+    console.log(`modified: ${worldNew.getManifolds().length}`);
 });
